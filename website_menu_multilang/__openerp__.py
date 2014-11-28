617c8aec5546dc0474957c6e240808647a482c58
Andrius Preimantas <andrius@versada.lt>
2014-11-28 10:13:08 +0200

    [FIX] Moved COPYRIGHT note to manifest; removed deprecated key from manifest

--- website_menu_multilang/__openerp__.py
+++ website_menu_multilang/__openerp__.py
@@ -1,6 +1,26 @@
-# -*- coding: utf-8 -*-
-# This file is part of OpenERP. The COPYRIGHT file at the top level of
-# this module contains the full copyright notices and license terms.
+##############################################################################
+#
+# OpenERP, Open Source Management Solution 
+# Copyright (C) 2014 by UAB Versada (Ltd.) <http://www.versada.lt> 
+# and contributors. See AUTHORS for more details.
+#
+# All Rights Reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 
 {
     'name': 'Website Menu Multilanguage',
@@ -27,7 +47,6 @@ TODO:
     'data': [
         'view/website.xml',
     ],
-    'update_xml': [],
     'installable': True,
     'application': False,
 }
