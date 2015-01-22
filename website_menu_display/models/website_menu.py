f534ee0cfe1c4c46245ea25295ac168f7fe0a7f9
Jordi Riera <jordi.riera@savoirfairelinux.com>
2015-01-22 12:37:17 -0500

    [ADD] Add website_menu_display module.

---
+++ website_menu_display/models/website_menu.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+#
+# OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import logging
+_logger = logging.getLogger(__name__)
+from openerp import models, fields
+from openerp.tools.translate import _
+
+
+class WebsiteMenu(models.Model):
+    """Improve website.menu with adding booleans that drive
+    if the menu is displayed when the user is logger or not.
+    """
+    _inherit = 'website.menu'
+
+    user_logged = fields.Boolean(
+        string="User Logged",
+        default=True,
+        help=_("If checked, the menu will be display when the user is logged.")
+    )
+
+    user_not_logged = fields.Boolean(
+        string="User Not Logged",
+        default=True,
+        help=_("If checked, "
+               "the menu will be display when the user is not logged.")
+    )
+
