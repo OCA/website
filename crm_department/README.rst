21868f55d00b737f5e6f5dff1589cea3e472ea31
Antonio Espinosa <antonioea@antiun.com>
2015-02-21 20:30:57 +0100

    Addon crm_department

---
+++ crm_department/README.rst
@@ -0,0 +1,13 @@
+Departments in CRM
+==================
+
+This module adds the concept of department to the CRM main objects (lead,
+opportunity, partner), and propagate them through the workflow.
+
+Credits
+=======
+
+Contributors
+------------
+* Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+* Antonio Espinosa <antonioea@antiun.com>
---
+++ crm_department/models/crm_department.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from openerp import models, fields
+
+
+class CrmDepartment(models.Model):
+    _name = 'crm.department'
+    _order = "parent_left"
+    _parent_order = "name"
+    _parent_store = True
+    _description = "Department"
+
+    name = fields.Char(required=True)
+    parent_id = fields.Many2one(comodel_name='crm.department')
+    children = fields.One2many(comodel_name='crm.department',
+                               inverse_name='parent_id')
+    parent_left = fields.Integer('Parent Left', select=True)
+    parent_right = fields.Integer('Parent Right', select=True)
