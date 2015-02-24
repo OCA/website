f7e9ab130e6f9f14b0a1765f98d535174bf7c504
Laurent Mignon <laurent.mignon@acsone.eu>
2015-02-24 16:40:42 +0100

    [ADD] website_hr_department New module to display the departments in the website

---
+++ website_hr_department/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#     This file is part of website_hr_department, an Odoo module.
+#
+#     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
+#
+#     website_hr_department is free software: you can redistribute it and/or
+#     modify it under the terms of the GNU Affero General Public License
+#     as published by the Free Software Foundation, either version 3 of
+#     the License, or (at your option) any later version.
+#
+#     website_hr_department is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Affero General Public License for more details.
+#
+#     You should have received a copy of the
+#     GNU Affero General Public License
+#     along with website_hr_department.
+#     If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': "Departments Page",
+    'summary': """
+        Display the structure of your departments and their members.
+        """,
+    'author': "ACSONE SA/NV",
+    'website': "http://acsone.eu",
+    'category': 'Website',
+    'version': '0.1',
+    'license': 'AGPL-3',
+    'depends': [
+        'website_hr',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/website_hr_department.xml',
+        'data/websiste_hr_department_data.xml',
+        'views/website_hr_department.xml',
+    ],
+}
