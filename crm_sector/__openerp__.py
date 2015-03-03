a96572bdeb95bfc8451ff3ea64c4cafdf0671545
Antonio Espinosa <antonioea@antiun.com>
2015-03-03 15:49:21 +0100

    CRM Sector access rights

--- crm_sector/__openerp__.py
+++ crm_sector/__openerp__.py
@@ -34,6 +34,7 @@
         'views/crm_sector_view.xml',
         'views/crm_lead_view.xml',
         'views/res_partner_view.xml',
+        'security/ir.model.access.csv',
     ],
     "installable": True,
 }
