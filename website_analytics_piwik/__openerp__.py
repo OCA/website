1fa57ad194735cc7f6b4170d2c296d9c8b3ce4c3
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2015-03-20 16:58:14 +0100

    Add Odoo Community Association (OCA) in authors

--- website_analytics_piwik/__openerp__.py
+++ website_analytics_piwik/__openerp__.py
@@ -21,7 +21,7 @@
 {
     "name": "Piwik analytics",
     "version": "1.0",
-    "author": "Therp BV",
+    "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Website",
     "summary": "Track website users using piwik",
