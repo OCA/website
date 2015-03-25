14d8a28ca535b2d0050155a297f1f0dbddb9d095
Antonio Espinosa <antonioea@antiun.com>
2015-03-25 10:08:48 +0100

    [ADD] crm_heading addon

---
+++ crm_heading/README.rst
@@ -0,0 +1,13 @@
+Heading in CRM
+==============
+
+This module allows to set heading for companies and contacts in the
+CRM main objects (lead, opportunity, partner), and propagate them through
+the workflow.
+
+Credits
+=======
+
+Contributors
+------------
+* Antonio Espinosa <antonioea@antiun.com>
---
+++ crm_heading/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Odoo Source Management Solution
+#    Copyright (c) 2015 Antiun Ingeniería S.L. (http://www.antiun.com)
+#                       Antonio Espinosa <antonioea@antiun.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Heading in CRM',
+    'version': '1.0',
+    'category': 'Customer Relationship Management',
+    'author': 'Antiun Ingeniería S.L.',
+    'website': 'http://www.antiun.com',
+    'depends': [
+        'crm',
+    ],
+    'data': [
+        'views/crm_heading_view.xml',
+        'views/crm_lead_view.xml',
+        'views/res_partner_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    "installable": True,
+}
