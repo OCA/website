f1c1b457e640031cb7d9e1d38034bac7dbfccdc8
Antonio Espinosa <antonioea@antiun.com>
2015-03-30 17:29:52 +0200

    Turnover items has no parent

--- crm_capital/models/crm_turnover_range.py
+++ crm_capital/models/crm_turnover_range.py
@@ -7,14 +7,6 @@ from openerp import models, fields
 
 class CrmTurnoverRange(models.Model):
     _name = 'crm.turnover_range'
-    _order = "parent_left"
-    _parent_order = "name"
-    _parent_store = True
     _description = "Turnover range"
 
     name = fields.Char(required=True, translate=True)
-    parent_id = fields.Many2one(comodel_name='crm.turnover_range')
-    children = fields.One2many(comodel_name='crm.turnover_range',
-                               inverse_name='parent_id')
-    parent_left = fields.Integer('Parent Left', select=True)
-    parent_right = fields.Integer('Parent Right', select=True)
