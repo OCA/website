3a4e02ae7efaa0dff272917c374db7e924688165
Antonio Espinosa <antonioea@antiun.com>
2015-03-30 17:30:12 +0200

    Employees range items has no parent

--- crm_employees/models/crm_employees_range.py
+++ crm_employees/models/crm_employees_range.py
@@ -7,14 +7,6 @@ from openerp import models, fields
 
 class CrmEmployeesRange(models.Model):
     _name = 'crm.employees_range'
-    _order = "parent_left"
-    _parent_order = "name"
-    _parent_store = True
     _description = "Employees range"
 
     name = fields.Char(required=True, translate=True)
-    parent_id = fields.Many2one(comodel_name='crm.employees_range')
-    children = fields.One2many(comodel_name='crm.employees_range',
-                               inverse_name='parent_id')
-    parent_left = fields.Integer('Parent Left', select=True)
-    parent_right = fields.Integer('Parent Right', select=True)
