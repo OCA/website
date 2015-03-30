4823abdc0956c183a7afe650f3dba517da35107b
Antonio Espinosa <antonioea@antiun.com>
2015-03-30 17:30:24 +0200

    Heading items has no parents

--- crm_heading/models/crm_heading.py
+++ crm_heading/models/crm_heading.py
@@ -7,14 +7,6 @@ from openerp import models, fields
 
 class CrmHeading(models.Model):
     _name = 'crm.heading'
-    _order = "parent_left"
-    _parent_order = "name"
-    _parent_store = True
     _description = "Heading"
 
     name = fields.Char(required=True, translate=True)
-    parent_id = fields.Many2one(comodel_name='crm.heading')
-    children = fields.One2many(comodel_name='crm.heading',
-                               inverse_name='parent_id')
-    parent_left = fields.Integer('Parent Left', select=True)
-    parent_right = fields.Integer('Parent Right', select=True)
