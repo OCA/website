54600991c674bebaece7ecd75da1179b00bdd682
Markus Schneider <markus.schneider@initos.com>
2015-04-02 14:55:19 +0200

    add website_sale_catseo module

---
+++ website_sale_catseo/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 initOS GmbH & Co. KG (<http://www.initos.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': "SEO for Product Categorie",
+    'summary': """add Public Product Categories CMS Blocks""",
+    'author': "initOS GmbH & Co. KG,OpenAT,Odoo Community Association (OCA)",
+    'website': "http://www.initos.com/",
+    'category': 'website',
+    'version': '0.1',
+    'depends': [
+        'website_sale',
+    ],
+    'installable': True,
+    'css': [
+        ],
+    'data': [
+        'views/templates.xml',
+    ],
+    'js': [
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
