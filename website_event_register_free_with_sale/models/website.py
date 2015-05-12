4fc32f489ee0a4d8ad2edb854971f968c363c2cd
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-05-12 19:08:11 +0200

    [FIX] website_event_register_free_with_sale: Don't omit context argument

--- website_event_register_free_with_sale/models/website.py
+++ website_event_register_free_with_sale/models/website.py
@@ -26,8 +26,8 @@ class Website(models.Model):
 
     @api.multi
     def sale_get_order(self, force_create=False, code=None,
-                       update_pricelist=None):
+                       update_pricelist=None, context=None):
         res = super(Website, self).sale_get_order(
             force_create=force_create, code=code,
-            update_pricelist=update_pricelist)
+            update_pricelist=update_pricelist, context=context)
         return res if res is not None else self.env['sale.order']
