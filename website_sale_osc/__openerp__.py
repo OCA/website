356826d116d8c3d195dacb7a8b1b0ea08a028d9e
Benjamin Bachmann <bbachmann@bloopark.de>
2015-05-13 12:09:38 +0200

    [ADD] improved description and and updaten readme

--- website_sale_osc/__openerp__.py
+++ website_sale_osc/__openerp__.py
@@ -21,12 +21,17 @@
 {
     'name': 'One Step Checkout',
     'category': 'Website',
-    'summary': 'Provide an All-In-One Checkout for Your Customer',
+    'summary': 'Provide an All-In-One Checkout for Your Odoo Customer',
     'version': '1.0',
     'description': """
 odoo One Step Checkout
 ======================
-Provide an All-In-One Checkout for Your Customer
+One Step Checkout combines all Odoo Checkout steps into one and removes all unnecessary fields and
+questions. Never before has check- out been easier and faster!
+
+Improving the checkout process results in more customers completing their sales, and this has an immediate impact on your bottom line. It is the single most effective technical change you can make to reduce shopping cart abandonment.
+
+Regards Bloopark
     """,
     'author': "bloopark systems GmbH & Co. KG",
     'website': "http://www.bloopark.de",
