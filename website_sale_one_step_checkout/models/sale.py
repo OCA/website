387b3f523ee0d70f80d61d8374b72617ce7e7138
Benjamin Bachmann <bbachmann@bloopark.de>
2015-06-12 12:07:36 +0200

    [FIX] pep8 fix

--- website_sale_one_step_checkout/models/sale.py
+++ website_sale_one_step_checkout/models/sale.py
@@ -18,7 +18,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp import api, fields, models
+from openerp import api, models
 
 
 class SaleOrder(models.Model):
