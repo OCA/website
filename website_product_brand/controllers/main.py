17c8dd3932836686cdc5e723a7caf7165f903fbe
MeetSCS <m.dholakia.serpentcs@gmail.com>
2015-06-17 13:08:42 +0530

    [IMP]Improved code to pass test_flake.

--- website_product_brand/controllers/main.py
+++ website_product_brand/controllers/main.py
@@ -26,7 +26,7 @@ from openerp.http import request
 import openerp.addons.website_sale.controllers.main
 from openerp import SUPERUSER_ID
 from openerp.addons.website.models.website import slug
-from openerp.addons.website_sale.controllers.main import table_compute, QueryURL
+from openerp.addons.website_sale.controllers.main import table_compute,QueryURL
 
 PPG = 20
 PPR = 4
@@ -187,11 +187,11 @@ class WebsiteSale(openerp.addons.website_sale.controllers.main.website_sale):
                        'compute_currency': compute_currency,
                        'keep': keep,
                        'style_in_product': lambda style,
-                       product: style.id in [s.id for s in 
-                                             product.website_style_ids],
-                       'attrib_encode': lambda attribs: 
-                                        werkzeug.url_encode([('attrib',i) for
-                                                              i in attribs])})
+                            product: style.id in [s.id for s in
+                            product.website_style_ids],
+                       'attrib_encode': lambda attribs:
+                            werkzeug.url_encode([('attrib', i) for
+                                                 i in attribs])})
         return request.website.render('website_sale.products', values)
 
     # Method to get the brands.
@@ -210,7 +210,8 @@ class WebsiteSale(openerp.addons.website_sale.controllers.main.website_sale):
         if post.get('search'):
             domain += [('name', 'ilike', post.get('search'))]
         brand_ids = brand_obj.search(cr, SUPERUSER_ID, domain)
-        for brand_rec in brand_obj.browse(cr, SUPERUSER_ID, brand_ids, context=context):
+        for brand_rec in brand_obj.browse(cr, SUPERUSER_ID, 
+                                          brand_ids, context=context):
             brand_values.append(brand_rec)
 
         keep = QueryURL('/page/product_brands', brand_id=[])
