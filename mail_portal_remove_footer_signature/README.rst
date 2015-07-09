03c3ff2bc73376aed1ff532ea6063e40eea82d53
Antonio Espinosa <antespi@gmail.com>
2015-07-09 19:16:21 +0200

    [ADD] Remove portal email footer signature addon

---
+++ mail_portal_remove_footer_signature/README.rst
@@ -0,0 +1,33 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :alt: AGPLv3 License
+
+Remove portal email footer signature
+====================================
+
+This module remove footer signature from emails, when portal addon is installed
+
+Credits
+=======
+
+Contributors
+------------
+
+* Rafael Blasco <rafabn@antiun.com>
+* Antonio Espinosa <antonioea@antiun.com>
+
+Maintainer
+----------
+
+.. image:: http://www.antiun.com/images/logo.png
+   :alt: Antiun Ingeniería S.L.
+   :target: http://www.antiun.com
+
+This module is maintained by Antiun Ingeniería S.L.
+
+Antiun Ingeniería S.L. is an IT consulting company especialized in Odoo
+and provides Odoo development, install, maintenance and hosting
+services.
+
+To contribute to this module, please visit https://github.com/Antiun
+or contact us at comercial@antiun.com
+
---
+++ mail_portal_remove_footer_signature/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Python source code encoding : https://www.python.org/dev/peps/pep-0263/
+##############################################################################
+#
+#    OpenERP, Odoo Source Management Solution
+#    Copyright (c) 2015 Antiun Ingeniería S.L. (http://www.antiun.com)
+#                       Antonio Espinosa <antonioea@antiun.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Remove portal email footer signature',
+    'category': 'Personalization',
+    'version': '1.0',
+    'depends': [
+        'mail_remove_footer_signature',
+        'portal',
+    ],
+    'author': 'Antiun Ingeniería S.L.',
+    'website': 'http://www.antiun.com',
+    'license': 'AGPL-3',
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': True,
+}
