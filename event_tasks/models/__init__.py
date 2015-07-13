471c70b969a1d9a4cfc1b9ecc99b9c5addc7eae6
Antonio Espinosa <antespi@gmail.com>
2015-07-13 11:23:55 +0200

    [REM] event_tasks features integrated into event_project addon

--- event_tasks/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-
-from . import event_event
-from . import project_task
