200016fbffd61518fbade4fcaa3e2b67bdb125da
Jairo Llopis <j.llopis@grupoesoc.es>
2015-07-30 09:55:34 +0200

    Add module website_container_fluid.

---
+++ website_container_fluid/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+{
+    "name": "Snippet container width type chooser",
+    "summary": "Let you choose between fixed or fluid containers",
+    "version": "8.0.1.0.0",
+    "category": "Website",
+    "license": "AGPL-3",
+
+    "website": "https://odoo-community.org/project/website-maintainers-79",
+    "author": "Grupo ESOC, Odoo Community Association (OCA)",
+
+    "application": False,
+    "installable": True,
+
+    "depends": [
+        "website",
+    ],
+    "data": [
+        "views/snippets.xml",
+    ],
+    "demo": [
+        "demo/pages.xml",
+    ],
+}
