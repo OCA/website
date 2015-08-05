15679cb93b66d8d5dbcb1f9d61acb16881028c16
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-08-05 12:40:26 +0200

    [REM] mail_compose_select_lang: Move to OCA (https://github.com/OCA/social/pull/2)

--- mail_compose_select_lang/wizard/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-from . import mail_compose_message
