9b24e10977667737e687178e00ae3fb969b9dc22
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-08-25 20:53:29 +0200

    [FIX] website_menu_by_user_status: Fix error E402

--- website_menu_by_user_status/models/website_menu.py
+++ website_menu_by_user_status/models/website_menu.py
@@ -19,9 +19,11 @@
 #
 
 import logging
-_logger = logging.getLogger(__name__)
 from openerp import models, fields
 from openerp.tools.translate import _
+
+
+_logger = logging.getLogger(__name__)
 
 
 class WebsiteMenu(models.Model):
