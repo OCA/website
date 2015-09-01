d9ce846e7a2ac419ccd04d69e653a7dbb19f445c
Antonio Espinosa <antespi@gmail.com>
2015-09-01 11:58:15 +0200

    [REM] Move mass_mailing_partner to OCA/crm

--- mass_mailing_partner/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# License AGPL-3 - See LICENSE file on root folder for details
-##############################################################################
-
-from . import mail_mass_mailing_contact
-from . import res_partner
-from . import mail_mass_mailing
