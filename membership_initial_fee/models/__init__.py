08616d4b15a6644431b94b7aac40d72f3703e090
Antonio Espinosa <antespi@gmail.com>
2015-09-04 18:30:39 +0200

    [REM] membership_initial_fee moved to OCA/vertical-association

--- membership_initial_fee/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# (c) 2015 Pedro M. Baeza
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from . import product_template
-from . import account_invoice
-from . import res_partner
--- membership_initial_fee/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# (c) 2015 Pedro M. Baeza
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from . import test_membership_initial_fee
