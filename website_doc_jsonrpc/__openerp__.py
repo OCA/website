44c573dc7a6c594adb1ffe1540caf421d6462172
Markus Schneider <markus.schneider@initos.com>
2015-09-21 15:42:41 +0200

    add website_doc_josonrpc module

---
+++ website_doc_jsonrpc/__openerp__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2010-2013 OpenERP s.a. (<http://openerp.com>).
+#    Copyright (C) 2014 initOS GmbH & Co. KG (<http://www.initos.com>).
+#    Author Markus Schneider <markus.schneider at initos.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Website Odoo JSON RPC doc',
+    'version': '0.1',
+    'author': 'initOS GmbH & Co. KG',
+    'category': '',
+    'description': """Generates a documentation for JSON RPC
+    for the current installation on the fly""",
+    'website': 'http://www.initos.com',
+    'license': 'AGPL-3',
+    'images': [
+    ],
+    'depends': [
+        'website'
+    ],
+    'data': [
+        'views/model_view.xml'
+    ],
+    'js': [
+    ],
+    'qweb': [
+    ],
+    'css': [
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
