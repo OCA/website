9647666a0b4e4f7f6fbabd3afc5cf08fb8cb9cbe
sergio-incaser <sergio@incaser.es>
2015-10-20 11:04:10 +0200

    [ADD] website_auth_supplier: New Module.

---
+++ website_auth_supplier/models/res_users.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Antiun Ingeniería S.L. - Sergio Teruel
+# (c) 2015 Antiun Ingeniería S.L. - Carlos Dauden
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from openerp import models, api
+
+
+class ResUsers(models.Model):
+    _inherit = "res.users"
+
+    @api.model
+    def _signup_create_user(self, values):
+        res = super(ResUsers, self)._signup_create_user(values)
+        if isinstance(res, int):
+            user = self.env['res.users'].browse(res)
+            account_type = values.get('account_type', False)
+            if account_type and account_type == 'supplier':
+                user.partner_id.supplier = True
+        return res
