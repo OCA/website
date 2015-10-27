e41a08f0852c05a0014505cf01a97662eb340e5c
Jairo Llopis <yajo.sk8@gmail.com>
2015-10-27 16:35:00 +0100

    Restructure website_cookie_notice.
    This will allow automatic translations and easier maintenance.
    
    Many code has been removed to simplify eveything.

--- website_cookie_notice/controllers/main.py
+++ website_cookie_notice/controllers/main.py
@@ -3,6 +3,7 @@
 #
 #    Copyright (C) 2015 Agile Business Group sagl (<http://www.agilebg.com>)
 #    Copyright (C) 2015 Lorenzo Battistini <lorenzo.battistini@agilebg.com>
+#    Copyright (C) 2015 Antiun Ingeniería S.L. <http://antiun.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
@@ -19,38 +20,12 @@
 #
 ##############################################################################
 
-from openerp.addons.web import http
-from openerp.http import request
-
-CONFIG_FIELDS = [
-    'cookieAnalytics',
-    'cookieMessage',
-    'cookiePolicyLink',
-    'cookieOverlayEnabled',
-    'cookieAnalyticsMessage',
-    'cookieErrorMessage',
-    'cookieDeclineButton',
-    'cookieAcceptButton',
-    'cookieResetButton',
-    'cookieWhatAreTheyLink',
-    'cookieAcceptButtonText',
-    'cookieDeclineButtonText',
-    'cookieResetButtonText',
-    ]
+from openerp import http
 
 
 class CookieNotice(http.Controller):
-    @http.route(
-        ['/cookie_notice/get_config'], type='json', auth="public",
-        website=True)
-    def get_cookie_notice_config(self):
-        cr, uid, context, pool = (
-            request.cr, request.uid, request.context, request.registry)
-        user_model = pool['res.users']
-        company_model = pool['res.company']
-        company_id = user_model._get_company(cr, uid, context)
-        company = company_model.browse(cr, uid, company_id, context)
-        res = {}
-        for field in CONFIG_FIELDS:
-            res[field] = getattr(company, field)
-        return res
+    @http.route("/website_cookie_notice/ok", auth="public")
+    def accept_cookies(self):
+        """Stop spamming with cookie banner."""
+        http.request.httpsession["accepted_cookies"] = True
+        return http.local_redirect("/")
