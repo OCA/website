a8169dd5add74c4492ce838c7cc81c4eaaacea29
eLBati <lorenzo.battistini@agilebg.com>
2015-11-10 16:59:33 +0100

    IMP README
    Conflicts:
    	website_cookie_notice/__openerp__.py
    
    IMP using JSON and avoiding cache breaking rendered pages
    
    Conflicts:
    	website_cookie_notice/views/website.xml
    
    FIX Duplicate key 'version' in dictionary and copyright headers
    
    FIX module version
    
    FIX author Tecnativa
    
    FIX website_cookie_notice: make installable

--- website_cookie_notice/controllers/main.py
+++ website_cookie_notice/controllers/main.py
@@ -1,31 +1,19 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2015 Agile Business Group sagl (<http://www.agilebg.com>)
-#    Copyright (C) 2015 Lorenzo Battistini <lorenzo.battistini@agilebg.com>
-#    Copyright (C) 2015 Antiun Ingeniería S.L. <http://antiun.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015-2016 Lorenzo Battistini - Agile Business Group
+# © 2015 Antiun Ingeniería S.L. <http://antiun.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import http
 
 
 class CookieNotice(http.Controller):
-    @http.route("/website_cookie_notice/ok", auth="public")
+    @http.route(
+        "/website_cookie_notice/ok", auth="public", website=True, type='json',
+        methods=['POST'])
     def accept_cookies(self):
         """Stop spamming with cookie banner."""
         http.request.httpsession["accepted_cookies"] = True
-        return http.local_redirect("/")
+        http.request.env['ir.ui.view'].search([
+            ('type', '=', 'qweb')
+            ]).clear_caches()
+        return {'result': 'ok'}
