52c39af23cad7b7a651fdac3ad1ddc235b52ccc8
eLBati <lorenzo.battistini@agilebg.com>
2015-11-10 16:59:33 +0100

    IMP README
    Conflicts:
    	website_cookie_notice/__openerp__.py
    
    IMP using JSON and avoiding cache breaking rendered pages
    
    Conflicts:
    	website_cookie_notice/views/website.xml
    
    FIX Duplicate key 'version' in dictionary and copyright headers
    
    FIX module version
    
    FIX author Tecnativa
    
    FIX website_cookie_notice: make installable

--- website_cookie_notice/static/src/js/accept_cookies.js
+++ website_cookie_notice/static/src/js/accept_cookies.js
@@ -1,15 +1,23 @@
 /* © 2015 Antiun Ingeniería, S.L.
+ * © 2015 Lorenzo Battistini - Agile Business Group
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  */
 
-"use strict";
-(function ($) {
-    $(".cc-cookies .btn-primary").click(function(event){
-        event.preventDefault();
-        $.ajax($(event.target).attr("href"), {
-            "complete": function(jqXHR, textStatus){
-                $(event.target).closest(".cc-cookies").hide("fast");
-            }
+odoo.define('website_cookie_notice.cookie_notice', function (require) {
+    "use strict";
+
+    var ajax = require('web.ajax');
+    var base = require('web_editor.base');
+
+    base.ready().done(function() {
+        $(".cc-cookies .btn-primary").click(function(e) {
+            e.preventDefault();
+            ajax.jsonRpc('/website_cookie_notice/ok', 'call').then(function (data) {
+                if (data.result == 'ok') {
+                    $(e.target).closest(".cc-cookies").hide("fast");
+                }
+            });
         });
     });
-})(jQuery);
+}
+);
