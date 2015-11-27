6146c201ac6989bdbb9a3421c98f975738c272eb
Antonio Espinosa <antespi@gmail.com>
2015-11-27 13:47:25 +0100

    [ADD] master_website addon

---
+++ master_website/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3: Antiun Ingenieria S.L. - Antonio Espinosa
+# See README.rst file on addon root folder for more details
+
+{
+    'name': "Antiun Master website",
+    'category': 'Personalization',
+    'version': '8.0.1.0.0',
+    'depends': [
+        'website_crm',
+        'mass_mailing_custom_unsubscribe',
+        'website_cookie_notice',
+        'website_crm_privacy_policy',
+        'website_crm_quick_answer',
+        'website_legal_page',
+        'website_logo',
+        'website_disable_odoo',
+    ],
+    'external_dependencies': {},
+    'data': [
+    ],
+    'author': 'Antiun Ingeniería',
+    'website': 'http://www.antiun.com',
+    'license': 'AGPL-3',
+    'installable': True,
+}
