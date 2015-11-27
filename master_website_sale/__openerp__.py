e8fd7e68f5c894c5e489c3a5f76d4b0e2288c645
Antonio Espinosa <antespi@gmail.com>
2015-11-28 00:54:52 +0100

    [ADD] master_website_sale addon

---
+++ master_website_sale/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3: Antiun Ingenieria S.L. - Antonio Espinosa
+# See README.rst file on addon root folder for more details
+
+{
+    'name': "Antiun Master website sale",
+    'category': 'Personalization',
+    'version': '8.0.1.0.0',
+    'depends': [
+        'website_sale_require_legal',
+    ],
+    'external_dependencies': {},
+    'data': [
+    ],
+    'author': 'Antiun Ingeniería',
+    'website': 'http://www.antiun.com',
+    'license': 'AGPL-3',
+    'installable': True,
+}
