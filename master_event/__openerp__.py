0575f5bf825fe19ab6ab6eb268fa94ec2a0ce1e1
Antonio Espinosa <antespi@gmail.com>
2015-11-28 00:49:59 +0100

    [ADD] master_event addon

---
+++ master_event/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3: Antiun Ingenieria S.L. - Antonio Espinosa
+# See README.rst file on addon root folder for more details
+
+{
+    'name': "Antiun Master event",
+    'category': 'Personalization',
+    'version': '8.0.1.0.0',
+    'depends': [
+        'partner_event',
+        'event_registration_mass_mailing',
+    ],
+    'external_dependencies': {},
+    'data': [
+    ],
+    'author': 'Antiun Ingeniería',
+    'website': 'http://www.antiun.com',
+    'license': 'AGPL-3',
+    'installable': True,
+}
