80596a7905d18669936aa06fd23d5ff927b0509f
gfcapalbo <giovanni@therp.nl>
2015-12-10 12:15:10 +0100

    Add modules

---
+++ website_snippet_blog_display_post/models/blog_post.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    This module copyright (C) 2015 Therp BV (<http://therp.nl>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import models
+
+
+class BlogPost(models.Model):
+    _inherit = 'blog.post'
+
+    def get_post(self, parameter_id, blog_id):
+        domain = [('blog_id', '=', blog_id)]
+        if parameter_id < 0:
+            return self.search(
+                domain, order='create_date desc',
+                limit=1, offset=-1-parameter_id)
+        else:
+            return self.browse([parameter_id])
+        return self.browse([])
