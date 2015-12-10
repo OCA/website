32848554cbc2bd17f4ba7291d8ada3e0b200483c
gfcapalbo <giovanni@therp.nl>
2015-12-10 12:28:16 +0100

    add module website_blog_no_background

---
+++ website_blog_no_background_image/README.rst
@@ -0,0 +1,32 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :alt: License: AGPL-3
+
+No background images in website Blog
+================================
+For every blogpost , allows to choose if the user wants the "Giant image"
+at the top of the blog post, or just a plain text blogpost
+with the title as top element.
+
+The default (with or without image) can be chosen when creating a blog.
+The user can override the default on the single blog post.
+
+In the same blog the user can mix posts with the large header
+image and without.
+
+
+Usage
+=====
+
+When creating a new blog by choosing the checkbox,
+Default blog-wide setting for background image"
+you are selecting the default for all new blog posts.
+
+When creating a blog entry in the backend choose 
+with the checkbox if you want the image.
+
+
+Contributors
+------------
+
+Giovanni Francesco Capalbo  <giovanni@etherp.nl>
+
---
+++ website_blog_no_background_image/models/blog_blog.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    This module copyright (C) 2015 Therp BV (<http://therp.nl>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import models, fields
+
+
+class WebsiteBlog(models.Model):
+
+    _inherit = 'blog.blog'
+
+    def _get_image_options(self):
+        return [('no_image', "No header image"),
+                ('small_image', "Show small header image"),
+                ('big_image', "Show big header image")
+                ]
+
+    background_image_show = fields.Selection(
+        string="Default blog-wide setting for background image",
+        selection=lambda self: self._get_image_options(),
+        default='no_image',
+        required=True,
+        help="allows to set default header image formatting "
+             "of the posts of this blog, the setting "
+             "can be changed per blog entry later.")
