632d8f4fcc47a9256895281e83c73fd276b17747
MeetSCS <m.dholakia.serpentcs@gmail.com>
2015-12-14 15:28:26 +0530

    [IMP]Improved changes as per oca standards.

--- website_snippet_image_gallery/README.rst
+++ website_snippet_image_gallery/README.rst
@@ -2,15 +2,16 @@
     :alt: License: AGPL-3
 
 Website Snippet Image Gallery
-==================================
+=============================
 
 Description
------------
+===========
 
-This module was written to add the functionality of image gallery in website with hover effects.
+This module adds the functionality of image gallery in website with hover effects.
+
 
 Usage
-------------
+=====
 
 To use this module, you need to:
 
@@ -22,6 +23,17 @@ https://www.odoo.com/forum/help-1
 Credits
 =======
 
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues <https://github.com/OCA/ website/issues>`_.
+In case of trouble, please check there if your issue has already been reported.
+If you spotted it first, help us smashing it by providing a detailed and
+welcomed feedback `here <https://github.com/OCA/
+website/issues/new?body=module:%20 website_snippet_image_gallery%0Aversion:%20
+8.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+
+
 Contributors
 ------------
 
@@ -31,9 +43,9 @@ Contributors
 Maintainer
 ----------
 
-.. image:: http://odoo-community.org/logo.png
+.. image:: https://odoo-community.org/logo.png
    :alt: Odoo Community Association
-   :target: http://odoo-community.org
+   :target: https://odoo-community.org
 
 This module is maintained by the OCA.
 
--- website_snippet_image_gallery/__openerp__.py
+++ website_snippet_image_gallery/__openerp__.py
@@ -1,37 +1,16 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2012-Today Serpent Consulting Services Pvt. Ltd.
-#                                     (<http://www.serpentcs.com>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>
-#
-##############################################################################
+# © 2015 Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Website Snippet Image Gallery',
     'category': 'Website',
     'author': 'Serpent Consulting Services Pvt. Ltd.',
+    "author": """Serpent Consulting Services Pvt. Ltd.,
+                 Odoo Community Association (OCA)""",
     'website': 'http://www.serpentcs.com',
     'summary': 'Adds image gallery in website.',
     'version': '8.0.1.0.0',
-    'description': """
-Website Snippet Image Gallery
-==================================
-Allows to add image gallery snippet in website with hover effect.
-        """,
     'depends': [
         'website',
     ],
