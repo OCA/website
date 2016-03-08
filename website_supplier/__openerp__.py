476658b4c5cbaf57e70f03fb49d868a0b05ee572
Michael Viriyananda <viriyananda.michael@gmail.com>
2016-03-08 15:47:38 +0700

    Add module website_supplier

---
+++ website_supplier/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2016 Michael Viriyananda
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Website Supplier',
+    'version': '8.0.1.0.0',
+    'summary': 'Publish Your Supplier References',
+    'author': 'Michael Viriyananda,Odoo Community Association (OCA)',
+    'website': 'https://github.com/mikevhe18',
+    'category': 'Website',
+    'depends': [
+        'purchase',
+        'website_partner',
+        'website_google_map'
+        ],
+    'data': [
+        'views/website_supplier.xml',
+        'views/res_partner_view.xml'
+    ],
+    'installable': True,
+}
