81a688ea6d936878b79d40467865bb07cad91e84
Dave Lasley <dave@laslabs.com>
2016-04-20 08:13:49 -0700

    Upgrade website_cookie_notice to 9.0

--- website_cookie_notice/static/src/js/load_cookiecuttr.js
+++ website_cookie_notice/static/src/js/load_cookiecuttr.js
@@ -1,12 +1,9 @@
-/**
- * Copyright (C) 2015 Lorenzo Battistini <lorenzo.battistini@agilebg.com>
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- */
-$(document).ready(function () {
-  $.cookieCuttr();
+// Copyright (C) 2015 Lorenzo Battistini <lorenzo.battistini@agilebg.com>
+// License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+odoo.define('website_cookie_notice.load_cookiecuttr', function(require) {
+  'use strict';
+  require('web_editor.base').ready().then(function() {
+    $.cookieCuttr();
+  });
 });
