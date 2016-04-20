ed2c26420f8baa9645f769f6c0a1de6dfdd469d9
Dave Lasley <dave@laslabs.com>
2016-04-20 08:44:21 -0700

    Upgrade website_legal_page to 9.0

--- website_legal_page/__init__.py
+++ website_legal_page/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
-# Python source code encoding : https://www.python.org/dev/peps/pep-0263/
-##############################################################################
+# © 2015 Antiun Ingeniería S.L. (http://www.antiun.com)
+# © 2015 Antonio Espinosa <antonioea@antiun.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
