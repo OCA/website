286addb0cd73fcec075bac9be308979630f8adec
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-05-05 19:13:30 +0200

    [IMP] website_anchor_smooth_scroll: Add author

--- website_anchor_smooth_scroll/__openerp__.py
+++ website_anchor_smooth_scroll/__openerp__.py
@@ -7,7 +7,9 @@
     "version": "8.0.1.0.0",
     "category": "Website",
     "website": "http://www.antiun.com",
-    "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
+    "author": "Antiun Ingeniería S.L., "
+              "Tecnativa, "
+              "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
