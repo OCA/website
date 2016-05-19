f1c4564cf8e0c3112a7c378296dac85f390b839a
Jairo Llopis <yajo.sk8@gmail.com>
2016-05-19 12:03:52 +0200

    [8.0][ADD][website_seo_redirection] Choose your URLs!
    This module allows you to modify any URL that points to any controller to whatever you want.
    
    LGPLv3 license.

---
+++ website_seo_redirection/models/ir_http.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# © 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from openerp import models
+from openerp.http import request
+from ..exceptions import NoOriginError, NoRedirectionError
+
+
+class IrHttp(models.AbstractModel):
+    _inherit = "ir.http"
+
+    def _dispatch(self):
+        """Handle SEO-redirected URLs."""
+        # Only handle redirections for HTTP requests
+        if not hasattr(request, "jsonrequest"):
+            wsr = request.env["website.seo.redirection"]
+
+            try:
+                # Redirect user to SEO version of this URL if possible
+                return wsr.redirect_auto()
+            except NoRedirectionError:
+                try:
+                    # Make Odoo believe it is in the original controller
+                    return self.reroute(wsr.find_origin())
+                except NoOriginError:
+                    pass
+
+        return super(IrHttp, self)._dispatch()
