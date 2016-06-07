278a065d765be87ca2619f40071115055f15c2d4
ralwafaie <rami.alwafaie@initos.de>
2016-06-07 11:07:52 +0200

    FIX `website_canonical_url` review remarks
    bug fixes + add languages
    
    clean the code
    
    fix README file

--- website_canonical_url/__openerp__.py
+++ website_canonical_url/__openerp__.py
@@ -1,46 +1,21 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Authors: Thomas Rehn
-#    Copyright (c) 2015 initOS GmbH & Co. KG (<http://www.initos.com>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#############################################################################
+# © initOS GmbH 2016
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
     'name': "Website Canoncial URL",
-
     'summary': """
-        Canonical URL in Website Headers""",
-
-    'author': "initOS GmbH & Co. KG",
+        Canonical URL in Website Headers
+        """,
+    'author': "initOS GmbH, Odoo Community Association (OCA)",
     'website': "http://www.initos.com",
-
     'category': 'Website',
-    'version': '0.1',
+    'version': '8.0.1.0.0',
     'license': 'AGPL-3',
-
     'depends': [
         'website',
     ],
     'data': [
         'views/templates.xml',
-    ],
-
-    'demo': [
-    ],
-
-    'tests': [
     ],
 }
