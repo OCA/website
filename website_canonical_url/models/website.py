a75f20aaa0c383c0086f0d588a8a5e86231f65fc
ralwafaie <rami.alwafaie@initos.de>
2016-06-07 14:04:30 +0200

    bug fixes + add languages

--- website_canonical_url/models/website.py
+++ website_canonical_url/models/website.py
@@ -4,6 +4,7 @@
 
 from openerp import models, api
 from openerp.http import request
+from urlparse import urlparse, urlunparse
 
 
 class website(models.Model):
@@ -13,8 +14,15 @@ class website(models.Model):
     def get_canonical_url(self, req=None):
         canonical_url = None
         if req is None:
-            req = request.httprequest
-        if req and req.base_url:
-            canonical_url = req.base_url
-            print canonical_url
+            req = request
+        if req and req.httprequest.base_url:
+            lang = self._context['lang']
+            if lang == request.website.default_lang_code:
+                canonical_url = req.httprequest.base_url
+            else:
+                url_parts = urlparse(req.httprequest.base_url)
+                url_parts = list(url_parts)
+                # change the path of the url
+                url_parts[2] = lang + url_parts[2]
+                canonical_url = urlunparse(url_parts)
         return canonical_url
