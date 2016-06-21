c13a1daf9383f6db866d51f3b35de8df7fad13d9
Dave Lasley <dave@laslabs.com>
2016-06-21 15:42:45 -0700

    [ADD] Create new module website_field_autocomplete

---
+++ website_field_autocomplete/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Website Field - AutoComplete",
+    "summary": 'Provides an autocomplete field for Website on any model',
+    "version": "9.0.1.0.0",
+    "category": "Website",
+    "website": "https://laslabs.com/",
+    "author": "LasLabs, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "website",
+    ],
+    "data": [
+        'views/assets.xml',
+    ],
+}
