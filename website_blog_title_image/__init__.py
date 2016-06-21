9e9ec8ad2b907bf9d9a9abf5cebcfa30c605f19e
tarteo <dennissluijk@outlook.com>
2016-06-21 09:58:33 +0200

    [IMP] Improved changing images and added backend view

--- website_blog_title_image/__init__.py
+++ website_blog_title_image/__init__.py
@@ -2,4 +2,4 @@
 # © 2016 ONESTEiN BV (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import models, controllers
+from . import models
--- website_blog_title_image/models/__init__.py
+++ website_blog_title_image/models/__init__.py
@@ -1,1 +1,5 @@
-import website_blog
+# -*- coding: utf-8 -*-
+# © 2016 ONESTEiN BV (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import website_blog
