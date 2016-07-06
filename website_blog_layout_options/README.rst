835560cc77d9f7c714a5a00cdba38775aabe749d
gfcapalbo <giovanni@therp.nl>
2016-07-06 13:37:56 +0200

    Add new website_blog_layout_options module, made by unifying smaller and ill-named website blog modules

---
+++ website_blog_layout_options/README.rst
@@ -0,0 +1,59 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+    :alt: License: AGPL-3
+
+===========================
+website_blog_layout_options
+===========================
+
+
+MORE DISPLAY TYPES
+------------------
+
+* Adds 2 extra display types of posts to the post list of any blog:
+
+1. Teaser: The title of the post with a small teaser and a "read more" link.
+2. Complete: The complete post in the post list
+
+
+* Blog posts can have thumbnails 
+
+The blogs can have a thumbnail, and the user can choose thumbnail size
+A thumbnail will be seen in the blog teaser and in the upper left corner of
+the blogpost itself.
+The thumbnail size can vary from small (64px wide) to gigantic (320px wide),
+the size of the thumbail will be the same for all blogposts.
+Some blogposts may have thumbnails other may have none (ala wordpress style).
+
+* Blogposts can have different size header images
+
+For every blogpost , allows to choose if the user wants the "Giant image"
+at the top of the blog post, or just a plain text blogpost
+with the title as top element.
+
+The default (with or without image) can be chosen when creating a blog.
+The user can override the default on the single blog post.
+
+In the same blog the user can mix posts with the large header
+image and without.
+
+
+Contributors
+------------
+
+* Giovanni francesco Capalbo <giovanni@therp.nl>
+* Holger Brunn <hbrunn@therp.nl>
+
+
+Maintainer
+----------
+
+.. image:: http://odoo-community.org/logo.png
+    :alt: Odoo Community Association
+    :target: http://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose mission is to support the collaborative development of Odoo features and promote its widespread use.
+
+To contribute to this module, please visit http://odoo-community.org.
---
+++ website_blog_layout_options/models/blog_post.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    This module copyright (C) 2015 Therp BV (<http://therp.nl>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp import models, fields, api
+from lxml import etree
+
+
+class BlogPost(models.Model):
+
+    _inherit = 'blog.post'
+
+    @api.one
+    @api.onchange('extract_auto', 'display_type', 'content', 'teaser_input')
+    def _extract_teaser(self):
+        if self.display_type == "teaser":
+            # no empty teasers
+            if (self.teaser_input or '') and (not self.extract_auto):
+                self.teaser = self.teaser_input
+            else:
+                res = ""
+                # limit length to roughly 3-4 lines.
+                teaser_length = 500
+                parser = etree.HTMLParser()
+                if self.content:
+                    tree = etree.fromstring(self.content, parser)
+                    paragraphs = tree.xpath('//p')
+                    # get the first non empty paragraph
+                    for paragraph in paragraphs:
+                        if paragraph.text and len(res) < teaser_length:
+                            res = res + paragraph.text + '\n'
+                        else:
+                            break
+                    # trim it to the intended length
+                    self.teaser = res[:teaser_length] + " ..."
+                    self.teaser_input = res[:teaser_length] + " ..."
+                else:
+                    # has no teaser or content,  just revert.
+                    # frontend controls needed not to have a bad workflow.
+                    # content cannot be inserted in backend by default.
+                    # add content to backend in view
+                    self.display_type = "no_teaser"
+
+    @api.onchange('blog_id')
+    def set_new_default(self):
+        self.background_image_show = self.blog_id.background_image_show
+
+    background_image_show = fields.Selection(
+        string="Type of header image on blog post",
+        selection=lambda self: self.blog_id._get_image_options(),
+        default=lambda self: self.blog_id.background_image_show,
+        required=True,
+        help="Choose if how you want to display the blog post:"
+        "Just the title above the post, a small header image"
+        "above the blog post title, or a big full screen image,"
+        "before showing the post, (odoo default)")
+
+    thumbnail = fields.Many2one(
+        string='Blog Post Thumbnail',
+        comodel_name='ir.attachment',
+        help='A small image shown in teaser and content'
+    )
+
+    display_type = fields.Selection(
+        selection=lambda self: self.blog_id._get_display_types(),
+        string='Display type',
+        default=lambda self: self.blog_id.display_type or 'no_teaser',
+        required=True,
+        help="Select no_teaser if you just want the clickable title,"
+             "in the list. Select Teaser if you want to display,"
+             "title+first lines of post select Complete if you prefer,"
+             "the entire text  to be viewed in the blog list.")
+
+    teaser = fields.Text(string='Teaser for Blog Post',
+                         compute="_extract_teaser")
+
+    teaser_input = fields.Text(string="Teaser text")
+    extract_auto = fields.Boolean(
+        string="Create teaser from content", default=False)
