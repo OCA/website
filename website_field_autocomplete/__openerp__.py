4ffcf42d72c56af86c8d9c8ca31a94fc34d35b4e
Dave Lasley <dave@laslabs.com>
2016-07-08 18:29:41 -0700

    [IMP] website_field_autocomplete: Upgrade for modularity * Break controller getters into two methods to allow for easier inheritance manipulation * Add method in JS to return autocomplete initialization args * Add self.record_ids to represent searched RecordSet * Update request type to JSON in AJAX

--- website_field_autocomplete/__openerp__.py
+++ website_field_autocomplete/__openerp__.py
@@ -5,7 +5,7 @@
 {
     "name": "Website Field - AutoComplete",
     "summary": 'Provides an autocomplete field for Website on any model',
-    "version": "9.0.1.0.0",
+    "version": "9.0.1.0.1",
     "category": "Website",
     "website": "https://laslabs.com/",
     "author": "LasLabs, Odoo Community Association (OCA)",
