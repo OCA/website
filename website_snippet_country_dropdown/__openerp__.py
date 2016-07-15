61a73bd0743e35b584010df71be32c8c6c68739f
Jairo Llopis <yajo.sk8@gmail.com>
2016-07-15 16:04:49 +0200

    [MIG] website_snippet_country_dropdown: Migrate to v9 + imp
    * Some minor improvements: fix a string, add placeholder support, document variables.
    * Acknowledge problem about drag and drop
    * Remove example link.

--- website_snippet_country_dropdown/__openerp__.py
+++ website_snippet_country_dropdown/__openerp__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 # © 2016 Sergio Teruel <sergio.teruel@tecnativa.com>
+# © 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    "name": "Website Snippet Country Dropdown Code",
+    "name": "Website Snippet Country Code Dropdown",
     "summary": "Allow to select country in a dropdown",
-    "version": "8.0.1.1.0",
+    "version": "9.0.1.0.0",
     "category": "Website",
     "website": "http://www.tecnativa.com",
     "author": "Tecnativa, "
