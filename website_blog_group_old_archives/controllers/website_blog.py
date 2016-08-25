326e94982f57dca7fd22bc8b0efd51116efd94fa
gfcapalbo <giovanni@therp.nl>
2016-08-25 12:33:51 +0200

    add controller

---
+++ website_blog_group_old_archives/controllers/website_blog.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# © 2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import datetime
+from openerp import http, tools
+from openerp.http import request
+from openerp.addons.website_blog.controllers.main import WebsiteBlog
+
+class website_blog(WebsiteBlog):
+
+    _blog_post_per_page = 20
+    _post_comment_per_page = 10
+
+    def nav_list_grouped(self):
+        blog_post_obj = request.registry['blog.post']
+        beginning_of_year = datetime.date.strftime(
+            datetime.date(datetime.date.today().year, 1, 1),
+            tools.DEFAULT_SERVER_DATE_FORMAT
+        )
+        old_groups = blog_post_obj.read_group(
+            request.cr, request.uid, 
+            [['create_date', '<', beginning_of_year]],
+            ['name', 'create_date'],
+            groupby="create_date", 
+            orderby="create_date desc", 
+            context=request.context
+        )       
+        new_groups = blog_post_obj.read_group(
+            request.cr, request.uid,
+            [['create_date', '>=', beginning_of_year]],
+            ['name', 'create_date'],
+            groupby="create_date:year", 
+            orderby="create_date desc", 
+            context=request.context
+        )
+        groups = new_groups + old_groups
+        for group in groups:
+            begin_date = datetime.datetime.strptime(
+                group['__domain'][0][2], tools.DEFAULT_SERVER_DATETIME_FORMAT
+            ).date()
+            end_date = datetime.datetime.strptime(
+                group['__domain'][1][2], tools.DEFAULT_SERVER_DATETIME_FORMAT
+            ).date()
+            group['date_begin'] = '%s' % datetime.date.strftime(
+                begin_date, tools.DEFAULT_SERVER_DATE_FORMAT
+            )
+            group['date_end'] = '%s' % datetime.date.strftime(
+                end_date, tools.DEFAULT_SERVER_DATE_FORMAT
+            )
+        return groups
+
+    @http.route()
+    def blog(self, blog=None, tag=None, page=1, **opt):
+        result = super(website_blog, self).blog(
+                blog=blog, tag=tag, page=page, **opt
+            )
+        result.qcontext['nav_list_old_grouped'] = self.nav_list_grouped()
+	return result
+
