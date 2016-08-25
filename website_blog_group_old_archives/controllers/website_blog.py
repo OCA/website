568a2897a8bdc8c06669aaf60bc36efa1f4b9ffb
gfcapalbo <giovanni@therp.nl>
2016-08-25 13:22:47 +0200

    final version

--- website_blog_group_old_archives/controllers/website_blog.py
+++ website_blog_group_old_archives/controllers/website_blog.py
@@ -18,24 +18,29 @@ class website_blog(WebsiteBlog):
             datetime.date(datetime.date.today().year, 1, 1),
             tools.DEFAULT_SERVER_DATE_FORMAT
         )
+        import pudb
+        pudb.set_trace()
         old_groups = blog_post_obj.read_group(
             request.cr, request.uid, 
             [['create_date', '<', beginning_of_year]],
             ['name', 'create_date'],
-            groupby="create_date", 
-            orderby="create_date desc", 
+            groupby="create_date:year", 
+            orderby="create_date:year desc", 
             context=request.context
         )       
+        # the standard orderby is just a comma separated list of fields
+        # optionally followed by desc or asc.
+        # UNDOCUMENTED FEATURE, the time :year, :month, :day work also 
+        # for orderby.
         new_groups = blog_post_obj.read_group(
             request.cr, request.uid,
             [['create_date', '>=', beginning_of_year]],
             ['name', 'create_date'],
-            groupby="create_date:year", 
+            groupby="create_date", 
             orderby="create_date desc", 
             context=request.context
         )
-        groups = new_groups + old_groups
-        for group in groups:
+        for group in new_groups:
             begin_date = datetime.datetime.strptime(
                 group['__domain'][0][2], tools.DEFAULT_SERVER_DATETIME_FORMAT
             ).date()
@@ -48,13 +53,29 @@ class website_blog(WebsiteBlog):
             group['date_end'] = '%s' % datetime.date.strftime(
                 end_date, tools.DEFAULT_SERVER_DATE_FORMAT
             )
-        return groups
+        for group in old_groups:
+            begin_date = datetime.datetime.strptime(
+                group['__domain'][0][2], tools.DEFAULT_SERVER_DATETIME_FORMAT
+            ).date()
+            end_date = datetime.datetime.strptime(
+                group['__domain'][1][2], tools.DEFAULT_SERVER_DATETIME_FORMAT
+            ).date()
+            group['date_begin'] = '%s' % datetime.date.strftime(
+                begin_date, tools.DEFAULT_SERVER_DATE_FORMAT
+            )
+            group['date_end'] = '%s' % datetime.date.strftime(
+                end_date, tools.DEFAULT_SERVER_DATE_FORMAT
+            )
+        return {'new_groups': new_groups, 'old_groups': old_groups}
 
     @http.route()
     def blog(self, blog=None, tag=None, page=1, **opt):
         result = super(website_blog, self).blog(
                 blog=blog, tag=tag, page=page, **opt
             )
-        result.qcontext['nav_list_old_grouped'] = self.nav_list_grouped()
+        import pudb
+        pudb.set_trace()
+        result.qcontext['nav_list_old_grouped'] = self.nav_list_grouped()['old_groups']
+        result.qcontext['nav_list_new_grouped'] = self.nav_list_grouped()['new_groups']
 	return result
 
