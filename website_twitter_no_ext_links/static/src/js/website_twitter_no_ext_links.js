dd07b6f785df499fb28343c7283ac67095b34fe2
gfcapalbo <giovanni@therp.nl>
2016-09-16 19:26:54 +0200

    Adding module website_twitter_no_ext_links

---
+++ website_twitter_no_ext_links/static/src/js/website_twitter_no_ext_links.js
@@ -0,0 +1,47 @@
+(function() {
+    "use strict";
+    var website = openerp.website;
+
+    website.snippet.options["jstweetfeed"] = openerp.website.snippet.Option.extend(
+    {   
+        select: function (event, np)
+        {   
+            var selection = event.$next.first().attr('data-value').split("_");
+            var content = this.$target;  
+            content.attr('data-tweettheme_id', selection[1]);
+            content.attr('data-tweetfeed_id', selection[0]);
+        },
+        start: function()
+        {   
+            this.$target.find('.parametricTemplate').html("click here to select twitter feed");
+        },
+        onFocus: function()
+        {  
+            this.$target.find('.parametricTemplate').html("Select twitter feed");
+        },
+        clean_for_save: function() 
+        {
+            this.$target.find('.parametricTemplate')
+                .empty()
+                .append(
+                    jQuery('<t />')
+                        .attr('t-call', 'website_twitter_no_ext_links.twitter_feed_content')
+                        .attr('t-ignore-branding', '1')
+                        .append(
+                            jQuery('<t />')
+                               .attr('t-value', this.$target.attr('data-tweetfeed_id'))
+                               .attr('t-set', 'tweetfeed_id')
+                               .attr('t-ignore-branding', '1')
+                             )    
+                        .append(
+                            jQuery('<t />')
+                               .attr('t-value', this.$target.attr('data-tweettheme_id'))
+                               .attr('t-set', 'tweettheme_id')
+                               .attr('t-ignore-branding', '1')
+                             )     
+                );
+        }
+    });
+
+})();
+
