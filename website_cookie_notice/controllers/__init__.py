1f2000b000a54f9a4f56dcd7602ef0401e39ef20
Alex Comba <alex.comba@agilebg.com>
2016-10-04 10:44:21 +0200

    [IMP] website_cookie_notice Update headers and README according to OCA conventions

--- website_cookie_notice/controllers/__init__.py
+++ website_cookie_notice/controllers/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2015-2016 Lorenzo Battistini - Agile Business Group
+# Copyright 2015-2016 Lorenzo Battistini - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import main
