cca0b0c7b7aafd2c043b6c847ed6e57a3ab28d7e
Dave Lasley <dave@laslabs.com>
2016-10-19 08:36:55 -0700

    [MIG] website_form_recaptcha: Upgrade to v10 * Update website_form_recaptcha to v10

--- website_form_recaptcha/__manifest__.py
+++ website_form_recaptcha/__manifest__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-# © 2016-TODAY LasLabs Inc.
+# Copyright 2016-2017 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Website Form - ReCaptcha",
     "summary": 'Provides a ReCaptcha field for Website Forms',
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "Website",
     "website": "https://laslabs.com/",
-    "author": "LasLabs, Odoo Community Association",
+    "author": "LasLabs, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
-    'installable': False,
+    'installable': True,
     "depends": [
         "website_form",
     ],
