33a665af9533fc4a3fc0402ccc366f0ff470248a
archetipo <alessio.gerace@gmail.com>
2016-10-31 11:35:03 +0100

    [MIG] website_signup_legal_page_required: Migrated to 9.

--- website_signup_legal_page_required/__openerp__.py
+++ website_signup_legal_page_required/__openerp__.py
@@ -5,17 +5,19 @@
 {
     "name": "Require accepting legal terms",
     "summary": "Force the user to accept the legal terms to open an account",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "category": "Website",
     "website": "http://www.antiun.com",
-    "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
+    "author": "Antiun Ingeniería S.L.,"
+    " Agile Business Group, Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "application": False,
-    "installable": True,
     "depends": [
         "website_legal_page",
     ],
     "data": [
         "views/pages.xml",
+        "views/res_users.xml",
     ],
+    "application": False,
+    "installable": True,
 }
