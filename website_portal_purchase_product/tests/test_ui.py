3020f34256afa1e7751f70eb3169b4bd2be619c5
Jairo Llopis <yajo.sk8@gmail.com>
2016-12-19 15:03:27 +0100

    Use dummy cursor to create tourman.

--- website_portal_purchase_product/tests/test_ui.py
+++ website_portal_purchase_product/tests/test_ui.py
@@ -8,19 +8,23 @@ from openerp.tests.common import HttpCase
 class UICase(HttpCase):
     def test_ui_website_portal_product_manager(self):
         """Test frontend tour."""
-        self.parent = self.env["res.partner"].create({
-            "name": "Tourman parent company",
-            "is_company": True,
-            "supplier": True,
-        })
-        self.tourman = self.env["res.users"].create({
-            "name": "Tourman",
-            "parent_id": self.parent.id,
-            "login": "tourman",
-            "new_password": "tourman",
-            "groups_id":
-                [(4, self.env.ref("base.group_portal").id, False)],
-        })
+        # See https://github.com/odoo/odoo/pull/13902
+        with self.cursor() as cr:
+            env = self.env(cr)
+            # Create a portal user to avoid dependency on portal module
+            self.parent = env["res.partner"].create({
+                "name": "Tourman parent company",
+                "is_company": True,
+                "supplier": True,
+            })
+            self.tourman = env["res.users"].create({
+                "name": "Tourman",
+                "parent_id": self.parent.id,
+                "login": "tourman",
+                "new_password": "tourman",
+                "groups_id":
+                    [(4, env.ref("base.group_portal").id, False)],
+            })
 
         self.phantom_js(
             url_path="/",
