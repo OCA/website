9839bdda16050e20a6281a644b76a893976e03ce
Jairo Llopis <yajo.sk8@gmail.com>
2016-12-19 11:40:43 +0100

    [9.0][website_seo_redirection] Fix tests.
    Fixes #296.

--- website_seo_redirection/models/website.py
+++ website_seo_redirection/models/website.py
@@ -2,6 +2,7 @@
 # © 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 from openerp import api, models
+from openerp.http import request
 
 
 class Website(models.Model):
@@ -20,11 +21,20 @@ class Website(models.Model):
             for url in record.origin, record.destination:
                 if url not in seo_redirections:
                     seo_redirections.append(url)
+        # Give super() a website to work with
+        if not request.website_enabled:
+            self.ensure_one()
+            request.website = self
+        # Yield super()'s pages
         for page in super(Website, self).enumerate_pages(query_string):
             try:
                 seo_redirections.remove(page["loc"])
             except ValueError:
                 pass
             yield page
+        # Remove website if we were supposed to have none
+        if not request.website_enabled:
+            request.website = None
+        # Yield redirected pages not detected by super()
         for page in seo_redirections:
             yield {"loc": page}
