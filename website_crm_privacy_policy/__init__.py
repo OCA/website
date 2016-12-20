9f3a5dad4906693c0adfd047b3cb03b5dd662434
cubells <vicent@vcubells.net>
2016-12-20 18:24:02 +0100

    website_crm_privacy_policy: Migration to 9.0

--- website_crm_privacy_policy/__init__.py
+++ website_crm_privacy_policy/__init__.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
-# Python source code encoding : https://www.python.org/dev/peps/pep-0263/
+# Copyright 2014 Jorge Camacho <jcamacho@trey.es>
+# Copyright 2015 Antonio Espinosa <antonioea@antiun.com>
+# Copyright 2016 Vicent Cubells <vicent.cubells@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 ##############################################################################
