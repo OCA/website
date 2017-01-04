ab01fe11ac0eb41fd3915a9ed2fe9e441a688d27
Jairo Llopis <yajo.sk8@gmail.com>
2017-01-04 13:00:25 +0100

    Remove JS. Use website_form's.
    New `website_form` addon includes an undocumented validation system.
    By simply adapting layout to what it wants, we get validation for free.

--- website_crm_privacy_policy/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2014 Jorge Camacho <jcamacho@trey.es>
-# Copyright 2015 Antonio Espinosa <antonioea@antiun.com>
-# Copyright 2016 Vicent Cubells <vicent.cubells@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-##############################################################################
