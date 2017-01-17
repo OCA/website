607eacc9acd47b9c323d711d1649c76a8ad3c40e
Vinay Bhawsar <siddharth@techspawn.com>
2017-01-17 10:52:00 +0530

    copyright removed

--- website_upload_video/__openerp__.py
+++ website_upload_video/__openerp__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2017 Techspawn Solutions (http://techspawn.com)
 # © 2016 ONESTEiN BV (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -9,7 +8,6 @@
     'category': 'Website',
     'version': '8.0.1.0.0',
     'author': 'ONESTEiN BV,Odoo Community Association (OCA)',
-              'Techspawn Solutions'
     'license': 'AGPL-3',
     'website': 'http://www.onestein.eu',
     'depends': ['website'],
--- website_upload_video/controllers/main.py
+++ website_upload_video/controllers/main.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2017 Techspawn Solutions (http://techspawn.com)
 # © 2016 ONESTEiN BV (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
