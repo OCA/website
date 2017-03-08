ece2a7aafebe9a7ad7ce42ac188d89f8369e1540
gilles <meyomesse.gilles@gmail.com>
2017-03-08 15:59:41 +0100

    [MIG] website_menu_by_user_status form v8 to v10

--- website_menu_by_user_status/__manifest__.py
+++ website_menu_by_user_status/__manifest__.py
@@ -1,28 +1,9 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    This module copyright (C) 2015 Savoir-faire Linux
-#    (<http://www.savoirfairelinux.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# -*- coding: utf-8 -*-
+# Copyright 2013-2017 Savoir-faire Linux (<http://www.savoirfairelinux.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Website Menu By User Display',
-    'version': '8.0.1.1.0',
+    'version': '10.0.1.0.0',
     'author': 'Savoir-faire Linux,Odoo Community Association (OCA)',
     'website': 'http://www.savoirfairelinux.com',
     'license': 'AGPL-3',
@@ -31,14 +12,9 @@
     'depends': [
         'website',
     ],
-    'external_dependencies': {
-        'python': [],
-    },
     'data': [
         'views/website_templates.xml',
         'views/website_views.xml',
     ],
-    'demo': [],
-    'test': [],
-    'installable': False,
+    'installable': True,
 }
