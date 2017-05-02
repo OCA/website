2bfe22ace995d2b37510b2cb1075a5fb1dea731b
Robin Chatfield <robin.chatfield@vertel.se>
2017-05-02 15:46:45 +0200

    website_menu_groups: Module created.

---
+++ website_menu_groups/__openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution, third party addon
+#    Copyright (C) 2004-2017 Vertel AB (<http://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Website Menu Groups',
+    'version': '0.1',
+    'category': '',
+    'description': """""",
+    'author': 'Vertel AB',
+    'website': 'http://www.vertel.se',
+    'depends': ['website'],
+    'data': [
+        'website_view.xml',
+        'security/website_security.xml',
+        ],
+    'application': False,
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4s:softtabstop=4:shiftwidth=4:
