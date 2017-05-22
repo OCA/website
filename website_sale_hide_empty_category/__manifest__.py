9c702b9f2c8b549d35bcdd56197288817b480c03
Ted Salmon <tsalmon@laslabs.com>
2017-05-22 12:12:55 -0700

    [ADD] website_sale_category_empty: Create module (#342)
    * [ADD] website_sale_category_empty: Create module
    * Add module to hide categories that have no products associated with them
    
    * [FIX] website_sale_hide_empty_category: PR Fix
    * Add trailing comma to manifest
    * Rename Module

---
+++ website_sale_hide_empty_category/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "Website Sale - Hide Empty Categories",
+    "summary": "Hide any Product Categories that are empty",
+    "version": "10.0.1.0.0",
+    "category": "Website",
+    "website": "https://laslabs.com/",
+    "author": "LasLabs, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "website_sale",
+    ],
+    "data": [
+        "views/website_sale_templates.xml",
+    ],
+}
---
+++ website_sale_hide_empty_category/models/product_public_category.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo import fields, models
+
+
+class ProductPublicCategory(models.Model):
+    _inherit = 'product.public.category'
+
+    # This module relies on the M2M relation built from `product.template` to
+    # `product.public.category` named `public_categ_ids`
+    product_ids = fields.Many2many(
+        comodel_name='product.template',
+        string='Products',
+        relation='product_public_category_product_template_rel',
+        column1='product_public_category_id',
+        column2='product_template_id',
+    )
