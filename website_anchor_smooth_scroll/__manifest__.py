0952b1f24ac213fe804f4b453f8561b19c484cfc
Alexandre Díaz <dev@redneboa.es>
2017-05-29 13:24:45 +0200

    [MIG] website_anchor_smooth_scroll: Migration to 10.0

---
+++ website_anchor_smooth_scroll/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2016 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Smooth Scroll for Website Anchors',
+    'summary': 'Replace default behavior for internal links (anchor only) with'
+               ' smooth scroll',
+    'version': '10.0.1.0.0',
+    'category': 'Website',
+    'website': 'https://www.tecnativa.com',
+    'author': 'Tecnativa, '
+              'LasLabs, '
+              'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'website',
+    ],
+    "data": [
+        "views/assets.xml",
+    ],
+    "demo": [
+        "demo/pages.xml",
+    ],
+}
