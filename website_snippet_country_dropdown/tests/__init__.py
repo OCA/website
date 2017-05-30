dc345198de7378224f35b7433c5ee068b5c226fc
Adrián del Río <a.delrio@ingetive.com>
2017-05-30 18:19:26 +0200

    [MIG] website_snippet_country_dropdown: Migration to 10.0

--- website_snippet_country_dropdown/tests/__init__.py
+++ website_snippet_country_dropdown/tests/__init__.py
@@ -2,4 +2,4 @@
 # © 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from . import tests
+from . import test_snippet_country_dropdown
