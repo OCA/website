979e48a820ef1a04e58d478f6fbfdd0561fcb948
ralwafaie <rami.alwafaie@initos.de>
2017-05-30 10:51:13 +0200

    Fix a bug

--- website_menu_url_translate/static/src/js/website.contentMenu.js
+++ website_menu_url_translate/static/src/js/website.contentMenu.js
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 initOS GmbH. <http://www.initos.com>
-# Copyright 2017 GYB IT SOLUTIONS <http://www.gybitsolutions.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+/*  Copyright 2017 initOS GmbH. <http://www.initos.com>
+ *  Copyright 2017 GYB IT SOLUTIONS <http://www.gybitsolutions.com>
+ *  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
 
 (function () {
     'use strict';
