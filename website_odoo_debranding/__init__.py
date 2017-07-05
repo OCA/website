acc586be7acb566713025c9cf0931762dff80eed
Jairo Llopis <yajo.sk8@gmail.com>
2017-07-05 11:50:36 +0200

    [MIG][website_odoo_debranding] Migrate to v10
    - Relicensed to LGPL.
    - The template that does the job can be disabled by the admin.

--- website_odoo_debranding/__init__.py
+++ website_odoo_debranding/__init__.py
@@ -1,3 +1,2 @@
 # -*- coding: utf-8 -*-
-# © 2016 Tecnativa, S.L. - Vicent Cubells
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
