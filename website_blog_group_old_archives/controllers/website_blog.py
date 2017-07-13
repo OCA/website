4632725a51ce21961200b8692fd1d4cc28db3326
Giovanni Francesco Capalbo <giovanni@therp.nl>
2017-07-13 10:20:04 +0200

    Fix issue 3901 flake8

--- website_blog_group_old_archives/controllers/website_blog.py
+++ website_blog_group_old_archives/controllers/website_blog.py
@@ -8,13 +8,13 @@ from openerp import http, tools
 from openerp.http import request
 from openerp.addons.website_blog.controllers.main import WebsiteBlog
 
+
 class website_blog(WebsiteBlog):
 
     _date_begin = None
     _date_end = None
     _tag = None
     _cat = None
-
 
     def nav_list_grouped(self):
         blog_post_obj = request.env['blog.post']
@@ -25,18 +25,18 @@ class website_blog(WebsiteBlog):
         old_groups = blog_post_obj.read_group(
             [['create_date', '<', beginning_of_year]],
             ['name', 'create_date'],
-            groupby="create_date:year", 
-            orderby="create_date:year desc", 
-        )       
+            groupby="create_date:year",
+            orderby="create_date:year desc",
+        )
         # the standard orderby is just a comma separated list of fields
         # optionally followed by desc or asc.
-        # UNDOCUMENTED FEATURE, the time :year, :month, :day work also 
+        # UNDOCUMENTED FEATURE, the time :year, :month, :day work also
         # for orderby.
         new_groups = blog_post_obj.read_group(
             [['create_date', '>=', beginning_of_year]],
             ['name', 'create_date'],
-            groupby="create_date", 
-            orderby="create_date desc", 
+            groupby="create_date",
+            orderby="create_date desc",
         )
         for group in new_groups:
             begin_date = datetime.datetime.strptime(
@@ -68,27 +68,36 @@ class website_blog(WebsiteBlog):
 
     @http.route()
     def blog(self, blog=None, tag=None, page=1, **opt):
-        self._date_begin, self._date_end = opt.get('date_begin'), opt.get('date_end')
+        self._date_begin, self._date_end = opt.get(
+            'date_begin'), opt.get('date_end')
         self._tag = tag
         result = super(website_blog, self).blog(
-                blog=blog, tag=tag, page=page, **opt)
-        result.qcontext['nav_list_old_grouped'] = self.nav_list_grouped()['old_groups']
-        result.qcontext['nav_list_new_grouped'] = self.nav_list_grouped()['new_groups']
-	return result
+            blog=blog, tag=tag, page=page, **opt)
+        result.qcontext[
+            'nav_list_old_grouped'] = self.nav_list_grouped()['old_groups']
+        result.qcontext[
+            'nav_list_new_grouped'] = self.nav_list_grouped()['new_groups']
+        return result
 
     @http.route()
-    def blog_post(self, blog, blog_post, tag_id=None, page=1, enable_editor=None, **post):
+    def blog_post(
+            self, blog, blog_post, tag_id=None, page=1,
+            enable_editor=None, **post):
         blog_post_obj = request.env['blog.post']
-        result = super(website_blog, self).blog_post(blog=blog,
-                blog_post=blog_post, tag_id=tag_id, page=page,
-                enable_editor=enable_editor, **post)
+        result = super(website_blog, self).blog_post(
+            blog=blog, blog_post=blog_post, tag_id=tag_id, page=page,
+            enable_editor=enable_editor, **post
+        )
         domain = []
         if blog:
             domain += [('blog_id', '=', blog.id)]
         if not tag_id and self._tag:
             domain += [('tag_ids', 'in', self._tag.id)]
         if self._date_begin and self._date_end:
-            domain += [("create_date", ">=", self._date_begin), ("create_date", "<=", self._date_end)]
+            domain += [
+                ("create_date", ">=", self._date_begin),
+                ("create_date", "<=", self._date_end)
+            ]
         if self._cat:
             domain += [('category_id', '=', self._cat.id)]
 
@@ -100,12 +109,16 @@ class website_blog(WebsiteBlog):
         # but if the blogpost id is not present in the current domain we
         # fetched we will just get a random one.
         if blog_post.id not in all_post_ids:
-            current_blog_post_index = random.sample(all_post_ids, 1)[0]
+            current_blog_post_index = random.sample(
+                list(xrange(len(all_post_ids))), 1
+            )[0]
         else:
-           current_blog_post_index = all_post_ids.index(blog_post.id)
-        next_post_id = all_post_ids[0 if current_blog_post_index >= len(all_post_ids) - 1 \
-                            else current_blog_post_index + 1]
-        next_post = next_post_id and blog_post_obj.browse(next_post_id) or False
+            current_blog_post_index = all_post_ids.index(blog_post.id)
+        next_post_id = all_post_ids[
+            0 if current_blog_post_index >= len(
+                all_post_ids) - 1 else current_blog_post_index + 1]
+        next_post = next_post_id and blog_post_obj.browse(
+            next_post_id) or False
 
         result.qcontext['next_post'] = next_post
         return result
@@ -116,4 +129,4 @@ class website_blog(WebsiteBlog):
         result = super(website_blog, self).blogcat(
             blog=blog, cat=cat, page=page, **opt
         )
-	return result
+        return result
