efa0c55685129bd4106e3245099a05fe298222b8
Jairo Llopis <yajo.sk8@gmail.com>
2017-07-25 13:54:03 +0200

    [MIG][website_snippet_marginless_gallery] Migrate to v9
    - Relicense to LGPL.
    - Move tour to demo asset.
    - Remove precompiled CSS.
    - Update snippet options JS API.
    - Update tour's API and definition.

--- website_snippet_marginless_gallery/__openerp__.py
+++ website_snippet_marginless_gallery/__openerp__.py
@@ -2,14 +2,13 @@
 {
     "name": "Marginless Gallery Snippet",
     "summary": "Add a snippet to have a marginless image gallery",
-    "version": "8.0.1.0.1",
+    "version": "9.0.1.0.0",
     "category": "Website",
-    "license": "AGPL-3",
-
+    "license": "LGPL-3",
     "website": "https://odoo-community.org/project/website-maintainers-79",
     "author": "Grupo ESOC Ingeniería de Servicios, "
+              "Tecnativa, "
               "Odoo Community Association (OCA)",
-
     "application": False,
     "installable": True,
     "images": [
@@ -21,5 +20,8 @@
     "data": [
         "views/assets.xml",
         "views/snippets.xml",
+    ],
+    "demo": [
+        "demo/assets.xml",
     ],
 }
