2bcdf232b14b149e28b718b009c84cbfa4e45c2e
Jairo Llopis <yajo.sk8@gmail.com>
2017-07-25 13:54:03 +0200

    [MIG][website_snippet_marginless_gallery] Migrate to v9
    - Relicense to LGPL.
    - Move tour to demo asset.
    - Remove precompiled CSS.
    - Update snippet options JS API.
    - Update tour's API and definition.

--- website_snippet_marginless_gallery/static/src/js/website_snippet_marginless_gallery.editor.js
+++ website_snippet_marginless_gallery/static/src/js/website_snippet_marginless_gallery.editor.js
@@ -1,48 +1,53 @@
-/* © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
- * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
- */
+/* Copyright 2015-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
+ * License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html). */
 
-"use strict";
-(function ($) {
-    var prompt = openerp.website.prompt,
-        snippet = openerp.website.snippet,
-        _t = openerp._t;
+odoo.define('website_marginless_gallery.gallery', function(require) {
+    "use strict";
+    var _t = require("web.core")._t;
+    var Dialog = require("web.Dialog");
+    var options = require('web_editor.snippets.options');
+    var website = require("website.website");
 
-    snippet.options.marginless_gallery = snippet.Option.extend({
-        start: function () {
-            var self = this;
-            self._super();
-            self.$el.find(".js_change_height").click(function () {
-                return self.change_images_height();
-            });
+    return options.registry.marginless_gallery = options.Class.extend({
+        auto_remove: function () {
+            return this.$target.data("overlay")
+                .find(".oe_snippet_remove").click();
         },
 
         drop_and_build_snippet: function () {
-            var self = this;
-            self._super();
-            self.change_images_height().fail(function () {
-                self.$target.data("overlay")
-                    .find(".oe_snippet_remove").click()
-            });
+            this.change_images_height("click")
+                .fail($.proxy(this, "auto_remove"));
+            return this._super.apply(this, arguments);
         },
 
-        change_images_height: function () {
-            var self = this;
-            return prompt({
+        change_images_height: function (type) {
+            if (type !== "click") return;
+            return website.prompt({
                 window_title: _t("Height for all images in gallery"),
                 input: _t("Image height in pixels"),
 
-            }).then(function (answer) {
-                // Must be a number
-                answer = Number(answer);
-                if (isNaN(answer)) {
-                    return failure();
-                }
-
-                self.$target
-                    .find(".row > .img")
-                    .css("height", answer ? answer + "px" : "");
-            });
+            }).then(
+                $.proxy(this, "images_height_changed"),
+                $.proxy(this, "images_height_failed")
+            );
         },
+
+        images_height_changed: function (pixels) {
+            // Must be a number
+            pixels = Number(pixels);
+            if (isNaN(pixels)) {
+                return $.Deferred().reject(pixels);
+            }
+            this.$target
+                .find(".row > .img")
+                .css("height", pixels ? pixels + "px" : "");
+        },
+
+        images_height_failed: function (pixels) {
+            return pixels && Dialog.alert(
+                this,
+                _.str.sprintf(_t("%s is not a valid value"), pixels)
+            );
+        }
     });
-})(jQuery);
+});
--- website_snippet_marginless_gallery/static/src/js/website_snippet_marginless_gallery.tour.js
+++ website_snippet_marginless_gallery/static/src/js/website_snippet_marginless_gallery.tour.js
@@ -1,29 +1,21 @@
-/* © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
- * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
- */
+/* Copyright 2015-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
+ * License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html). */
 
-"use strict";
-(function ($) {
-    openerp.Tour.register({
+odoo.define("web_module_name.tour", function (require) {
+    "use strict";
+    var base = require("web_editor.base");
+    var Tour = require("web.Tour");
+
+    return base.ready().done($.proxy(Tour, "register", {
         id: "marginless_gallery",
         name: "Insert a marginless gallery snippet",
-        path: "/page/homepage",
+        path: "/",
         mode: "test",
         steps: [
             {
                 title: "Edit the homepage (1)",
                 waitFor: "button[data-action=edit]",
                 element: "button[data-action=edit]",
-            },
-            {
-                title: "Click on Insert Blocks (1)",
-                waitFor: "button[data-action=snippet]",
-                element: "button[data-action=snippet]",
-            },
-            {
-                title: "Click on Features (1)",
-                waitFor: "a[href='#snippet_feature']",
-                element: "a[href='#snippet_feature']",
             },
             {
                 title: "Drag and drop a marginless gallery snippet (1)",
@@ -36,19 +28,6 @@
                 element: ".modal button:contains('Cancel'):visible",
             },
             {
-                title: "Click on Insert Blocks (2)",
-                waitNot: ".modal:visible, \
-                          #oe_manipulators .btn:visible, \
-                          #wrap .marginless-gallery:visible",
-                waitFor: "button[data-action=snippet]",
-                element: "button[data-action=snippet]",
-            },
-            {
-                title: "Click on Features (2)",
-                waitFor: "a[href='#snippet_feature']",
-                element: "a[href='#snippet_feature']",
-            },
-            {
                 title: "Drag and drop a marginless gallery snippet (2)",
                 waitFor: ".oe_snippet:contains('Marginless Gallery'):visible",
                 snippet: ".oe_snippet:contains('Marginless Gallery')",
@@ -59,6 +38,11 @@
                 element: ".modal .btn-primary:visible",
             },
             {
+                title: "Click on the snippet to see options",
+                waitNot: ".modal:visible",
+                element: "#wrap .marginless-gallery",
+            },
+            {
                 title: "Click on Customize",
                 waitFor: "#wrap .marginless-gallery, \
                           .btn-primary:contains('Customize'):visible",
@@ -66,13 +50,12 @@
             },
             {
                 title: "Click on Change Images Height",
-                waitFor: ".js_change_height:visible",
-                element: ".js_change_height:visible",
+                element: "li[data-change_images_height]:visible a",
             },
             {
                 title: "Set image height of 200px",
-                waitFor: ".modal input:visible",
-                element: ".modal input:visible",
+                waitFor: ".modal-body input:visible",
+                element: ".modal-body input:visible",
                 sampleText: "200",
             },
             {
@@ -87,5 +70,5 @@
                           .col-md-3[style*='height: 200px']",
             },
         ],
-    });
-})(jQuery);
+    }));
+});
