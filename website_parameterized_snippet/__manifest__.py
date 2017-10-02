b0f7d4b60bfc3b172995636c3bb617158418dc69
Giovanni Francesco Capalbo <giovanni@therp.nl>
2017-10-02 17:37:05 +0200

    [UPD] #4932  migrate website_parametrized_snippet to 10

---
+++ website_parameterized_snippet/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# © 2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Parameterize snippets",
+    "version": "10.0.1.0.0",
+    "author": "Therp BV, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ['website'],
+    "category": "Website",
+    "installable": True,
+    "application": False,
+}
---
+++ website_parameterized_snippet/models/view_view.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# (C) 2015 Therp BV <http://therp.nl>.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+
+
+class IrUiView(models.Model):
+    _inherit = 'ir.ui.view'
+
+    def is_node_branded(self, node):
+        # We skip evaluation for these nodes
+        if(node.attrib.get('t-ignore-branding')):
+            return False
+        return super(
+            IrUiView, self).is_node_branded(node=node)
