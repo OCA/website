70f869f43357569d0b4b0f75332d1135606e968c
Damien Bouvy <dbo@odoo.com>
2017-10-30 20:14:44 +0100

    [MIG] website_form_recaptcha: Migrate to 11.0
    - Remove groups on ir_config_paramater records (gone in 11.0)
    - Move error mapping to private function to allow translation

--- website_form_recaptcha/__manifest__.py
+++ website_form_recaptcha/__manifest__.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-2017 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Website Form - ReCaptcha",
     "summary": 'Provides a ReCaptcha field for Website Forms',
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Website",
-    "website": "https://laslabs.com/",
+    "website": "https://github.com/OCA/website",
     "author": "LasLabs, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
