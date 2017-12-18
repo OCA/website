c705e2e98594c1f79a7f81ef9062317b7ed069f3
Jairo Llopis <yajo.sk8@gmail.com>
2017-12-18 11:24:28 +0100

    fixup! [ADD] website_form_builder: Exactly what the title says

--- website_form_builder/static/src/js/snippets.js
+++ website_form_builder/static/src/js/snippets.js
@@ -1,51 +1,33 @@
 /* Copyright 2017 Tecnativa - Jairo Llopis
  * License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl). */
 
-odoo.define('website_form_builder.snippets', function(require){
+odoo.define('website_form_builder.snippets', function (require) {
     "use strict";
 
+    var ajax = require("web.ajax");
     var base = require('web_editor.base');
     var core = require('web.core');
+    var data = require("web.data");
     var Model = require('web.Model');
     var options = require('web_editor.snippets.options');
-    var _t = core._t;
     var widgets = require("website_form_builder.widgets");
-    var data = require("web.data");
-
-    core.qweb.add_template(
-        "/website_form_builder/static/src/xml/snippets.xml"
-    );
+    var _t = core._t;
 
     var _fields_asked = {},
         _fields_def = {},
         _models_asked = false,
-        _models_def = $.Deferred();
-
-    /**
-     * Lazily load just once authorized fields for given model.
-     */
-    function authorized_fields (model) {
-        if (!_fields_asked[model]) {
-            _fields_def[model] = $.Deferred();
-            available_models().done(function (models) {
-                new Model("ir.model").call(
-                    "get_authorized_fields",
-                    [models[model].id],
-                    {context: base.get_context()}
-                ).done($.proxy(
-                    _fields_def[model].resolve,
-                    _fields_def[model]
-                ));
-            });
-            _fields_asked[model] = true;
-        }
-        return _fields_def[model];
-    }
+        _models_def = $.Deferred(),
+        _templates_loaded = ajax.loadXML(
+            "/website_form_builder/static/src/xml/snippets.xml",
+            core.qweb
+        );
 
     /**
      * Lazily ask just once for models.
+     *
+     * @returns {$.Deferred} Indicates models were loaded.
      */
-    function available_models () {
+    function available_models() {
         if (!_models_asked) {
             new Model("ir.model").call("search_read", {
                 domain: [
@@ -64,6 +46,30 @@ odoo.define('website_form_builder.snippets', function(require){
             _models_asked = true;
         }
         return _models_def;
+    }
+
+    /**
+     * Lazily load just once authorized fields for given model.
+     *
+     * @param {String} model Model technical name.
+     * @returns {$.Deferred} Indicates fields were loaded.
+     */
+    function authorized_fields(model) {
+        if (!_fields_asked[model]) {
+            _fields_def[model] = $.Deferred();
+            available_models().done(function (models) {
+                new Model("ir.model").call(
+                    "get_authorized_fields", [models[model].id], {
+                        context: base.get_context()
+                    }
+                ).done($.proxy(
+                    _fields_def[model].resolve,
+                    _fields_def[model]
+                ));
+            });
+            _fields_asked[model] = true;
+        }
+        return _fields_def[model];
     }
 
     var Field = options.Class.extend({
@@ -85,7 +91,7 @@ odoo.define('website_form_builder.snippets', function(require){
                 .children("span").prop("contentEditable", true);
         },
 
-        toggle_class: function (type, value, $li) {
+        toggle_class: function (type, value) {
             this._super.apply(this, arguments);
             // Toggle field required attribute to match the container class
             if (type === "reset" || value === "o_required") {
@@ -98,9 +104,15 @@ odoo.define('website_form_builder.snippets', function(require){
 
         /**
          * Prompt the user for a default value for this field.
+         *
+         * @param {String} type Event type
+         * @returns {Dialog} Opened dialog
          */
-        ask_default_value: function (type, value, $li) {
-            if (type === "reset") return; // Nothing to reset here
+        ask_default_value: function (type) {
+            if (type === "reset") {
+                // Nothing to reset here
+                return;
+            }
             var form = new widgets.DefaultValueForm(this, {}, this.$target);
             form.on("save", this, this.set_default_value);
             return form.open();
@@ -165,7 +177,9 @@ odoo.define('website_form_builder.snippets', function(require){
                 var $el = $(this),
                     $label = $el.children(".control-label"),
                     $input = $el.find(".o_website_form_input");
-                if (!$label.length) return;
+                if (!$label.length) {
+                    return;
+                }
                 $input.attr("name", _.str.clean($label.text()));
                 $input.filter(":checkbox, :radio").each(function () {
                     var $box = $(this);
@@ -184,10 +198,12 @@ odoo.define('website_form_builder.snippets', function(require){
             if (fields.length) {
                 // Whitelist model fields found in current form
                 new Model("ir.model.fields").call(
-                    "formbuilder_whitelist",
-                    [this.controller_data().model_name, fields],
-                    {context: base.get_context()},
-                    {async: false} // Do not save until done
+                    "formbuilder_whitelist", [this.controller_data().model_name, fields], {
+                        context: base.get_context()
+                    }, {
+                        // Do not save until done
+                        async: false
+                    }
                 );
             } else {
                 // No fields? Destroy snippet before saving
@@ -200,14 +216,20 @@ odoo.define('website_form_builder.snippets', function(require){
          */
         drop_and_build_snippet: function () {
             this.ask_model();
-            return this._super.apply(this, arguments);
+            this._super.apply(this, arguments);
         },
 
         /**
          * Let user set hidden data for the form.
+         *
+         * @param {String} type Event type
+         * @returns {Dialog} Opened dialog
          */
         ask_hidden_data: function (type) {
-            if (type === "reset") return; // Nothing to reset here
+            if (type === "reset") {
+                // Nothing to reset here
+                return;
+            }
             var form = new widgets.HiddenDataForm(
                 this, {}, this.controller_data().hidden_data
             );
@@ -217,6 +239,8 @@ odoo.define('website_form_builder.snippets', function(require){
 
         /**
          * Set form's hidden data.
+         *
+         * @param {Object} new_data Mapping of hidden data for the form.
          */
         set_hidden_data: function (new_data) {
             var old_data = this.controller_data().hidden_data;
@@ -225,21 +249,30 @@ odoo.define('website_form_builder.snippets', function(require){
                     this.$form.removeAttr("data-form_field_" + key);
                 }
             }
-            for (var key in new_data) {
+            for (key in new_data) {
                 this.$form.attr("data-form_field_" + key, new_data[key]);
             }
         },
 
         /**
          * Fetch available models and let user choose one.
+         *
+         * @param {String} type Event type
+         * @returns {$.Deferred} Resolves with the open form
          */
         ask_model: function (type) {
-            if (type === "reset") return; // Nothing to reset here
+            if (type === "reset") {
+                // Nothing to reset here
+                return;
+            }
             return available_models().done($.proxy(this._ask_model, this));
         },
 
         /**
          * Create and process form widget for asking the model.
+         *
+         * @param {Object} models ORM records of ir.model objects
+         * @returns {Dialog} Open dialog
          */
         _ask_model: function (models) {
             var form = new widgets.ParamsForm(
@@ -251,9 +284,15 @@ odoo.define('website_form_builder.snippets', function(require){
 
         /**
          * Fetch available fields from model and let user choose one.
+         *
+         * @param {String} type Event type
+         * @returns {$.Deferred} Resolves with the open dialog
          */
         ask_model_field: function (type) {
-            if (type === "reset") return; // Nothing to reset here
+            if (type === "reset") {
+                // Nothing to reset here
+                return;
+            }
             return authorized_fields(this.controller_data().model_name).done(
                 $.proxy(this._ask_model_field, this)
             );
@@ -261,6 +300,9 @@ odoo.define('website_form_builder.snippets', function(require){
 
         /**
          * Create and process form widget for choosing the new model field.
+         *
+         * @param {Array} fields Fields among which user can choose
+         * @returns {Dialog} Open dialog
          */
         _ask_model_field: function (fields) {
             var form = new widgets.ModelFieldForm(
@@ -272,9 +314,17 @@ odoo.define('website_form_builder.snippets', function(require){
 
         /**
          * Inject a new custom field into the form.
+         *
+         * @param {String} type Event type
+         * @param {String} value Custom field type to add
+         * @param {jQuery} $li Clicked menu item
+         * @returns {jQuery} Added field
          */
         add_custom_field: function (type, value, $li) {
-            if (type === "reset") return; // Nothing to reset here
+            if (type === "reset") {
+                // Nothing to reset here
+                return;
+            }
             var name = _.str.sprintf(
                     _t("Custom %s field"),
                     _.str.clean($li.text())
@@ -334,9 +384,7 @@ odoo.define('website_form_builder.snippets', function(require){
         },
 
         /**
-         * List present fields.
-         *
-         * @returns Array
+         * @returns {Array} List of present field names
          */
         present_fields: function () {
             return _.pluck(this.$(":input[name]"), "name");
@@ -344,6 +392,8 @@ odoo.define('website_form_builder.snippets', function(require){
 
         /**
          * Change form's target model.
+         *
+         * @param {String} model Technical model name
          */
         set_model: function (model) {
             var previous_model = this.controller_data().model_name;
@@ -362,14 +412,19 @@ odoo.define('website_form_builder.snippets', function(require){
 
         /**
          * Empty form's current fields and fill with only required ones.
+         *
+         * @param {Object} fields ORM ir.model.fields records
          */
         reset_model_fields: function (fields) {
             // Remove old model fields
             this.$(".o_website_form_fields [data-model-field=true]").remove();
             // Add new model required fields by default
-            for (name in fields) {
+            for (var name in fields) {
                 if (fields[name].required) {
-                    this.add_model_field({name: name, field: fields[name]});
+                    this.add_model_field({
+                        name: name,
+                        field: fields[name]
+                    });
                 }
             }
         },
@@ -377,7 +432,8 @@ odoo.define('website_form_builder.snippets', function(require){
         /**
          * Inject a new field from the model into the form.
          *
-         * @param {name: field_name, field: field_definition} info
+         * @param {Object} info {name: field_name, field: field_definition}
+         * @returns {$.Deferred} Resolves with the added field jQuery element
          */
         add_model_field: function (info) {
             var relational_data = [],
@@ -389,7 +445,7 @@ odoo.define('website_form_builder.snippets', function(require){
                 relational_data = this.relational_options(info.field);
             }
             return $.when(template, info.name, info.field, relational_data,
-                          true)
+                    true, _templates_loaded)
                 .done($.proxy(this._add_field, this));
         },
 
@@ -399,19 +455,21 @@ odoo.define('website_form_builder.snippets', function(require){
          * @param {String} template QWeb field template name to be rendered
          * @param {String} name Field name
          * @param {Object} field Field attributes
-         * @param {Array[Object]} relational_data Data for x2x fields
+         * @param {Array} relational_data Data for x2x fields
          * @param {Boolean} model_field Is it a model field?
+         * @returns {jQuery} Appended element
          */
         _add_field: function (template, name, field, relational_data,
-                              model_field) {
+            model_field) {
             return this.$(".o_website_form_fields").append(core.qweb.render(
-                template,
-                {
+                template, {
                     field: field,
                     model_field: model_field,
                     name: name,
                     relational_data: relational_data,
-                    required_att: field.required ? "required" : null,
+                    required_att: field.required
+                        ? "required"
+                        : null,
                     widget: this,
                 }
             ));
@@ -419,13 +477,18 @@ odoo.define('website_form_builder.snippets', function(require){
 
         /**
          * Selectable options for relational fields.
+         *
+         * @param {String} field Field name
+         * @returns {$.Deferred} ORM results
          */
         relational_options: function (field) {
-            var domain = [], context = base.get_context();
+            var domain = [],
+                context = base.get_context();
             // Domain might contain un-evaluable literals
             try {
                 domain = new data.CompoundDomain(field.domain || []).eval();
             } catch (error) {
+                // eslint-disable-next-line no-console
                 console.warn("Cannot evaluate field domain, ignoring.");
             }
             // Context too
@@ -435,6 +498,7 @@ odoo.define('website_form_builder.snippets', function(require){
                     field.context
                 ).eval();
             } catch (error) {
+                // eslint-disable-next-line no-console
                 console.warn("Cannot evaluate field context, using user's.");
             }
             // Get results
@@ -456,5 +520,5 @@ odoo.define('website_form_builder.snippets', function(require){
         available_models: available_models,
         Field: Field,
         Form: Form,
-    }
+    };
 });
--- website_form_builder/static/src/js/widgets.js
+++ website_form_builder/static/src/js/widgets.js
@@ -1,166 +1,201 @@
 /* Copyright 2017 Tecnativa - Jairo Llopis
  * License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl). */
 
-odoo.define('website_form_builder.widgets', function(require){
-    "use strict";
+ odoo.define('website_form_builder.widgets', function (require) {
+     "use strict";
+     // HACK https://github.com/eslint/eslint/issues/9857
+     /* eslint-disable no-useless-call */
 
-    var base = require('web_editor.base');
+    var ajax = require("web.ajax");
     var core = require('web.core');
-    var Model = require('web.Model');
-    var options = require('web_editor.snippets.options');
     var widget = require("web_editor.widget");
     var _t = core._t;
     var Dialog = widget.Dialog;
 
-    core.qweb.add_template("/website_form_builder/static/src/xml/widgets.xml");
+    var result = $.Deferred(),
+        _templates_loaded = ajax.loadXML(
+            "/website_form_builder/static/src/xml/widgets.xml",
+            core.qweb
+        );
 
-    return {
-        DefaultValueForm: Dialog.extend({
-            template: "website_form_builder.DefaultValueForm",
+    var DefaultValueForm = Dialog.extend({
+        template: "website_form_builder.DefaultValueForm",
 
-            /**
-             * Store needed field information.
-             *
-             * @param {DOMElement} $field DOM element of the field
-             * that wants a new default value.
-             */
-            init: function (parent, options, field) {
-                this.field_html = $(field).html();
-                options = $.extend({}, {
-                    title: _t("Set field's default value"),
-                    size: "small",
-                }, options);
-                return this._super.call(this, parent, options);
-            },
+        /**
+         * Store needed field information.
+         *
+         * @param {Object} parent Widget where this dialog is attached
+         * @param {Object} options Dialog creation options
+         * @param {DOMElement} field Field asking for a new default value
+         * @returns {Dialog} New Dialog object
+         */
+        init: function (parent, options, field) {
+            this.field_html = $(field).html();
+            var _options = $.extend({}, {
+                title: _t("Set field's default value"),
+                size: "small",
+            }, options);
+            return this._super.call(this, parent, _options);
+        },
 
-            /**
-             * Return the new default value.
-             */
-            save: function () {
-                var inputs = this.$(".o_website_form_input");
-                if (inputs.is(":checkbox")) {
-                    this.final_data = inputs.filter(":checked")
+        /**
+         * Save the new default value.
+         */
+        save: function () {
+            var inputs = this.$(".o_website_form_input");
+            if (inputs.is(":checkbox")) {
+                this.final_data = inputs.filter(":checked")
                     .map(function () {
                         return $(this).val();
                     })
                     .get();
-                } else {
-                    this.final_data = inputs.val();
+            } else {
+                this.final_data = inputs.val();
+            }
+            this._super.apply(this, arguments);
+        },
+    });
+
+    var HiddenDataForm = Dialog.extend({
+        template: "website_form_builder.HiddenDataForm",
+
+        /**
+         * Store current data before creating widget
+         *
+         * @param {Object} parent Widget where this dialog is attached
+         * @param {Object} options Dialog creation options
+         * @param {Object} current_data Preexisting form hidden data
+         * @returns {Dialog} New Dialog object
+         */
+        init: function (parent, options, current_data) {
+            this.current_data = "";
+            for (var key in current_data) {
+                this.current_data += key + ":" + current_data[key] + "\n";
+            }
+            var _options = $.extend({}, {
+                title: _t("Hidden data"),
+            }, options);
+            return this._super.call(this, parent, _options);
+        },
+
+        /**
+         * Save new hidden data
+         */
+        save: function () {
+            var data = this.$("#data").val().split("\n");
+            this.final_data = {};
+            for (var line in data) {
+                line = data[line];
+                var combination = line.match(/^([^:]+):(.*)$/);
+                if (!combination) {
+                    continue;
                 }
-                return this._super.apply(this, arguments);
-            },
-        }),
+                this.final_data[combination[1]] = combination[2];
+            }
+            this._super.apply(this, arguments);
+        },
+    });
 
-        HiddenDataForm: Dialog.extend({
-            template: "website_form_builder.HiddenDataForm",
+    var ParamsForm = Dialog.extend({
+        template: "website_form_builder.ParamsForm",
 
-            /**
-             * Store current data before creating widget
-             */
-            init: function (parent, options, current_data) {
-                this.current_data = "";
-                for (var key in current_data) {
-                    this.current_data += key + ":" + current_data[key] + "\n";
+        /**
+         * Store models info before creating widget
+         *
+         * @param {Object} parent Widget where this dialog is attached
+         * @param {Object} options Dialog creation options
+         * @param {Array} models Available models to choose among
+         * @param {String} chosen Prechosen model
+         * @returns {Dialog} New Dialog object
+         */
+        init: function (parent, options, models, chosen) {
+            this.models = models;
+            this.chosen = chosen;
+            var _options = $.extend({}, {
+                title: _t("Form Settings"),
+                size: "small",
+            }, options);
+            return this._super.call(this, parent, _options);
+        },
+
+        /**
+         * Save new model
+         */
+        save: function () {
+            this.final_data = this.$("#model").val();
+            this._super.apply(this, arguments);
+        },
+    });
+
+    var ModelFieldForm = Dialog.extend({
+        template: "website_form_builder.ModelFieldForm",
+
+        /**
+         * Store fields info before creating widget
+         *
+         * @param {Object} parent Widget where this dialog is attached
+         * @param {Object} options Dialog creation options
+         * @param {Array} fields Model's fields
+         * @param {Array} blacklist Fields that cannot be chosen
+         * @returns {Dialog} New Dialog object
+         */
+        init: function (parent, options, fields, blacklist) {
+            this.fields = fields;
+            this.blacklist = blacklist;
+            var _options = $.extend({}, {
+                title: _t("Add Model Field"),
+                size: "small",
+            }, options);
+            return this._super.call(this, parent, _options);
+        },
+
+        /**
+         * Save field dict
+         */
+        save: function () {
+            var name = this.$("#field").val();
+            this.final_data = {
+                name: name,
+                field: this.fields[name],
+            };
+            this._super.apply(this, arguments);
+        },
+
+        /**
+         * Get filtered fields sorted by string.
+         *
+         * @returns {Array} [[string, name], ...]
+         */
+        sorted_fields: function () {
+            var _result = [];
+            for (var name in this.fields) {
+                var field = this.fields[name];
+                if (
+                    name === "id" ||
+                    field.readonly ||
+                    field.type === "one2many" ||
+                    field.type === "reference" ||
+                    field.type === "serialized" ||
+                    this.blacklist.indexOf(name) !== -1
+                ) {
+                    continue;
                 }
-                options = $.extend({}, {
-                    title: _t("Hidden data"),
-                }, options);
-                return this._super.call(this, parent, options);
-            },
+                _result.push([field.string, name]);
+            }
+            _result.sort();
+            return _result;
+        },
+    });
 
-            /**
-             * Return new hidden data
-             */
-            save: function () {
-                var data = this.$("#data").val().split("\n");
-                this.final_data = {};
-                for (var line in data) {
-                    line = data[line];
-                    var combination = line.match(/^([^:]+):(.*)$/);
-                    if (!combination) continue;
-                    this.final_data[combination[1]] = combination[2];
-                }
-                return this._super.apply(this, arguments);
-            },
-        }),
+    // Resolve when finished loading templates
+    _templates_loaded.done(function () {
+        result.resolve({
+            DefaultValueForm: DefaultValueForm,
+            HiddenDataForm: HiddenDataForm,
+            ParamsForm: ParamsForm,
+            ModelFieldForm: ModelFieldForm,
+        });
+    });
 
-        ParamsForm: Dialog.extend({
-            template: "website_form_builder.ParamsForm",
-
-            /**
-             * Store models info before creating widget
-             */
-            init: function (parent, options, models, chosen) {
-                this.models = models;
-                this.chosen = chosen;
-                options = $.extend({}, {
-                    title: _t("Form Settings"),
-                    size: "small",
-                }, options);
-                return this._super.call(this, parent, options);
-            },
-
-            /**
-             * Return model name on save
-             */
-            save: function () {
-                this.final_data = this.$("#model").val();
-                return this._super.apply(this, arguments);
-            },
-        }),
-
-        ModelFieldForm: Dialog.extend({
-            template: "website_form_builder.ModelFieldForm",
-
-            /**
-             * Store fields info before creating widget
-             */
-            init: function (parent, options, fields, blacklist) {
-                this.fields = fields;
-                this.blacklist = blacklist;
-                options = $.extend({}, {
-                    title: _t("Add Model Field"),
-                    size: "small",
-                }, options);
-                return this._super.call(this, parent, options);
-            },
-
-            /**
-             * Return field dict on save.
-             */
-            save: function () {
-                var name = this.$("#field").val();
-                this.final_data = {
-                    name: name,
-                    field: this.fields[name],
-                };
-                return this._super.apply(this, arguments);
-            },
-
-            /**
-             * Get filtered fields sorted by string.
-             *
-             * @returns [[string, name], ...]
-             */
-            sorted_fields: function () {
-                var result = [];
-                for (var name in this.fields) {
-                    var field = this.fields[name];
-                    if (
-                        name === "id" ||
-                        field.readonly ||
-                        field.type === "one2many" ||
-                        field.type === "reference" ||
-                        field.type === "serialized" ||
-                        this.blacklist.indexOf(name) !== -1
-                    ) {
-                        continue;
-                    }
-                    result.push([field.string, name]);
-                }
-                result.sort();
-                return result;
-            },
-        })
-    };
+    return result;
 });
