06ff5f577128ad88de7aba812486cdfa7f1bb96a
Andrea <a.stirpe@onestein.nl>
2017-12-21 16:51:58 +0100

    [MIG][11.0] Porting website_analytics_piwik to V11

--- website_analytics_piwik/__init__.py
+++ website_analytics_piwik/__init__.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Therp BV <http://therp.nl>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import models
--- website_analytics_piwik/models/__init__.py
+++ website_analytics_piwik/models/__init__.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Therp BV <http://therp.nl>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import website
 from . import website_config_settings
