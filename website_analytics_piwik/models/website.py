b9b85cfd0ce20051eb508aebd1f8ae3faaea6669
Andrea <a.stirpe@onestein.nl>
2017-12-21 16:51:58 +0100

    [MIG][11.0] Porting website_analytics_piwik to V11

--- website_analytics_piwik/models/website.py
+++ website_analytics_piwik/models/website.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Therp BV <http://therp.nl>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields
 
@@ -8,6 +7,7 @@ from odoo import models, fields
 class Website(models.Model):
     _inherit = 'website'
 
+    has_piwik_analytics = fields.Boolean("Piwik Analytics")
     piwik_analytics_id = fields.Integer(
         'Piwik website ID', help='The ID Piwik uses to identify the website',
         default=1)
