a3d7dad9e19893a514f083ed608bb793b06465ae
tarteo <d.sluijk@onestein.nl>
2018-02-20 11:31:31 +0100

    [IMP] overriding method of inaccessible class

--- website_media_size/static/src/js/website_media_size.js
+++ website_media_size/static/src/js/website_media_size.js
@@ -2,8 +2,11 @@
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
 
 odoo.define('website_media_size', function(require) {
+"use strict";
+
     var MediaDialog = require('web_editor.widget').MediaDialog;
     var weContext = require("web_editor.context");
+    var Widget = require('web.Widget');
 
     MediaDialog.include({
         xmlDependencies: MediaDialog.prototype.xmlDependencies.concat(
@@ -11,31 +14,20 @@ odoo.define('website_media_size', function(require) {
         ),
         start: function() {
             var res = this._super.apply(this, arguments);
-            this.include_file_size(this.imageDialog);
-            this.include_file_size(this.documentDialog);
             this.search();
             return res;
-        },
-        include_file_size: function(obj) {
-            var self = this;
-            // ImageDialog of the web_editor.widget module is not available
-            obj.fetch_existing = function(needle) {
-                var domain = [['res_model', '=', 'ir.ui.view']].concat(obj.domain);
-                if (needle && needle.length) {
-                    domain.push('|', ['datas_fname', 'ilike', needle], ['name', 'ilike', needle]);
-                }
-                return self._rpc({
-                    model: 'ir.attachment',
-                    method: 'search_read',
-                    args: [],
-                    kwargs: {
-                        domain: domain,
-                        fields: ['name', 'mimetype', 'checksum', 'url', 'type', 'file_size'],
-                        order: [{name: 'id', asc: false}],
-                        context: weContext.get(),
-                    }
-                }).then(obj.proxy('fetched_existing'));
-            };
+        }
+    });
+
+    // ImageDialog of the web_editor.widget module is not available
+    Widget.include({
+        _rpc: function(params, options) {
+            if (this.template === 'web_editor.dialog.image' &&
+                params.model === 'ir.attachment' &&
+                params.method === 'search_read') {
+                params.kwargs.fields.push('file_size');
+            }
+            return this._super(params, options);
         }
     });
 });
