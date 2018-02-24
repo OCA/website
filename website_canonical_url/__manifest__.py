8fbd08b73c2d3127075c6b13f3c825a1807c4d6f
Xavier Brochard <xavier@alternatif.org>
2018-02-24 15:06:22 +0100

    change website url with OCA/website and (c) with "copyright"

--- website_canonical_url/__manifest__.py
+++ website_canonical_url/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-# © initOS GmbH 2016
+# Copyright initOS GmbH 2016
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': "Website Canonical URL",
     'summary': "Canonical URL in Website Headers",
     'author': "initOS GmbH, Tecnativa, Odoo Community Association (OCA)",
-    'website': "http://www.initos.com",
+    'website': "https://github.com/OCA/website",
     'category': 'Website',
     'version': '10.0.1.0.0',
     'license': 'AGPL-3',
