f4eea5bb41426530252ebf1c1faa599ad71ea4fc
David Dufresne <dufresne.david.2@gmail.com>
2018-02-27 10:17:54 -0500

    [MIG][11.0] website_menu_by_user_status
    Extending the controllers and the website xml templates is not required anymore because the
    field is_visible on website.menu is computed.

--- website_menu_by_user_status/models/website_menu.py
+++ website_menu_by_user_status/models/website_menu.py
@@ -1,14 +1,14 @@
-# -*- coding: utf-8 -*-
 # Copyright 2013-2017 Savoir-faire Linux (<http://www.savoirfairelinux.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, _
+from odoo import api, fields, models, _
 
 
 class WebsiteMenu(models.Model):
     """Improve website.menu with adding booleans that drive
     if the menu is displayed when the user is logger or not.
     """
+
     _inherit = 'website.menu'
 
     user_logged = fields.Boolean(
@@ -26,3 +26,15 @@ class WebsiteMenu(models.Model):
                "the menu will be displayed when the user is not logged "
                "and give access.")
     )
+
+    @api.one
+    def _compute_visible(self):
+        """Display the menu item whether the user is logged or not."""
+        super()._compute_visible()
+        if not self.is_visible:
+            return
+
+        if self.env.user == self.env.ref('base.public_user'):
+            self.is_visible = self.user_not_logged
+        else:
+            self.is_visible = self.user_logged
