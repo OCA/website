2cc8b6c52c2348768846277f37cae59ae76cd922
David Dufresne <dufresne.david.2@gmail.com>
2018-02-26 22:42:32 -0500

    [MIG][11.0] website_odoo_debranding

--- website_odoo_debranding/__manifest__.py
+++ website_odoo_debranding/__manifest__.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Tecnativa, S.L. - Vicent Cubells
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Remove odoo.com bindings on website",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
-    "website": "https://www.tecnativa.com",
+    "website": "https://github.com/OCA/website",
     "license": "LGPL-3",
     "category": "Website",
     "depends": [
