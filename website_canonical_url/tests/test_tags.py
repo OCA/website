71f6a753b3992acf18e2fad8c84eb6d903bbf538
Xavier Brochard <xavier@alternatif.org>
2018-03-04 14:38:50 +0100

    different url for canonical

--- website_canonical_url/tests/test_tags.py
+++ website_canonical_url/tests/test_tags.py
@@ -10,7 +10,8 @@ class UICase(HttpCase):
     def setUp(self):
         super(UICase, self).setUp()
         self.root = 'http://' + self.xmlrpc_db._ServerProxy__host
-        self.url = self.root + "/page/website_canonical_url.canonical_demo"
+        self.url = "/page/website_canonical_url.canonical_demo"
+        self.url_absolute = self.root + "/../page/website_canonical_url.canonical_demo"
         self.get = "?ultimate_answer=42"
         self.url_get = "%s%s" % (self.url, self.get)
         self.url_data = self.url_open(self.url_get)
@@ -19,7 +20,7 @@ class UICase(HttpCase):
     def test_canonical(self):
         """Canonical URL is built OK."""
         node = self.doc.xpath("/html/head/link[@rel='canonical']")[0]
-        self.assertEqual(node.attrib["href"], self.url)
+        self.assertEqual(node.attrib["href"], self.url_absolute)
 
     def test_pager_next(self):
         """Next pager link is OK."""
