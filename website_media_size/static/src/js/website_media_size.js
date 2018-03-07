bd905b8a4bba6c6bf0ed79dada2c9972966170be
tarteo <d.sluijk@onestein.nl>
2018-03-07 14:39:47 +0100

    [REM] Unnecessary inheritance

--- website_media_size/static/src/js/website_media_size.js
+++ website_media_size/static/src/js/website_media_size.js
@@ -5,21 +5,12 @@ odoo.define('website_media_size', function(require) {
     "use strict";
 
     var web_editor_widget = require('web_editor.widget');
-    var MediaDialog = web_editor_widget.MediaDialog;
     var ImageDialog = web_editor_widget.ImageDialog;
 
-    MediaDialog.include({
-        xmlDependencies: MediaDialog.prototype.xmlDependencies.concat(
+    ImageDialog.include({
+        xmlDependencies: ImageDialog.prototype.xmlDependencies.concat(
             ['/website_media_size/static/src/xml/website_media_size.xml']
         ),
-        start: function() {
-            var res = this._super.apply(this, arguments);
-            this.search();
-            return res;
-        }
-    });
-
-    ImageDialog.include({
         _rpc: function(params, options) {
             if (params.model === 'ir.attachment' &&
                 params.method === 'search_read') {
