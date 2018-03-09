2558d704cb9f37fe874a391665bfbe20f810f713
Jairo Llopis <yajo.sk8@gmail.com>
2018-03-09 09:19:01 +0000

    [FIX] website_multi_theme: Proper checking of special views inheritance

--- website_multi_theme/__manifest__.py
+++ website_multi_theme/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Website Multi Theme",
     "summary": "Support different theme per website",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.0.1",
     "category": "Website",
     "website": "https://www.tecnativa.com",
     "author": "Tecnativa, Odoo Community Association (OCA)",
