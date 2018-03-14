b6f9e05e6f3adef82a672897669a5e0e04d9614a
Ivan Yelizariev <yelizariev@it-projects.info>
2018-03-14 04:04:15 -0400

    [FIX] point inherit to copied views (#430)
    * [FIX] point inherit to copied views
    
    Otherwise theme_customize doesn't work when we have chain of views like in
    theme_clean:
    
    theme_clean.option_bg_shade_light2 inherits theme_clean.less
    theme_clean.less inherits website.assets_frontend
    
    without this update, website_multi_theme copies
    theme_clean.option_bg_shade_light2 but keeps inherit_id the same (i.e. to
    theme_clean.less, which is not used, because it has a copy)
    
    * [LINT] remove unused arguments, add semicolon

--- website_multi_theme/static/src/js/theme.js
+++ website_multi_theme/static/src/js/theme.js
@@ -12,7 +12,7 @@ odoo.define('website_multi_theme.theme', function(require){
     }
 
     theme.include({
-        update_style: function (enable, disable, reload) {
+        update_style: function () {
             var links = multi_theme_links();
             if (links.length) {
                 // Placeholder for dynamically-loaded assets upstream
@@ -28,5 +28,5 @@ odoo.define('website_multi_theme.theme', function(require){
 
     return {
         multi_theme_links: multi_theme_links,
-    }
+    };
 });
