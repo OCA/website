01ba1c3629cb8599b7e0f9b3bd119e87220743f6
David <david.vidal@tecnativa.com>
2018-05-23 09:03:36 +0200

    [ADD] website_form_metada: New module

---
+++ website_form_metadata/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 David Vidal <david.vidal@tecnativa.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Website Form Metadata',
+    'version': '10.0.1.0.0',
+    'category': 'Website',
+    'website': 'https://github.com/OCA/website',
+    'author': 'Tecnativa, '
+              'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'views/res_config_view.xml',
+    ],
+}
