766f2e276c525bcdb1b2dc257d35ac83443e5fe6
Simone Orsi <simahawk@gmail.com>
2018-06-14 13:15:09 +0200

    [11.0] Add `website_addthis`

---
+++ website_addthis/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2018 Camptocamp
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import models
---
+++ website_addthis/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2018 Camptocamp
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "AddThis integration",
+    "version": "11.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Website",
+    "summary": "Integrate addthis.com features in your website",
+    "website": "https://github.com/OCA/website",
+    "depends": [
+        "website",
+    ],
+    "data": [
+        "views/website_config_settings.xml",
+        "views/website.xml",
+        "templates/layout.xml",
+    ],
+    "installable": True,
+}
