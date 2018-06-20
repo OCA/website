f1fedf13aa1c29644a740593b542f65eab028315
tarteo <d.sluijk@onestein.nl>
2018-06-20 11:26:53 +0200

    [ADD] website_img_dimension

---
+++ website_img_dimension/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2018 Onestein
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Website Image Dimensions',
+    'summary': 'Shows the width and height of images on the website '
+               'when hovered upon.',
+    'category': 'Website',
+    'version': '11.0.1.0.0',
+    'author': 'Onestein, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/oca/website',
+    'depends': [
+        'website'
+    ],
+    'data': [
+        'templates/assets.xml'
+    ]
+}
