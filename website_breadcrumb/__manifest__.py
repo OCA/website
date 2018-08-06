5c93b16b7a8e1f539878cb7a37d6fb10243e50e9
Yenthe666 <yenthevg@gmail.com>
2018-08-06 12:16:13 +0200

    [MIG] website_breadcrumb: migrate to V11

---
+++ website_breadcrumb/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "Website Breadcrumbs",
+    "summary": "Let you have breadcrumbs in website pages",
+    "version": "11.0.0.0.1",
+    "category": "Website",
+    "website": "https://grupoesoc.es/",
+    "author": "Odoo Experts, "
+              "Grupo ESOC Ingeniería de Servicios, "
+              "Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "website",
+    ],
+    "data": [
+        "views/templates.xml",
+        "views/layout.xml",
+    ],
+}
---
+++ website_breadcrumb/models/website.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import api, models
+
+
+class WebsiteMenu(models.Model):
+    _inherit = "website.menu"
+
+    @api.multi
+    def get_parents(self, revert=False, include_self=False):
+        """List current menu's parents.
+
+        :param bool revert:
+            Indicates if the result must be revert before returning.
+            Activating this will mean that the result will be ordered from
+            parent to child.
+
+        :param bool include_self:
+            Indicates if the current menu item must be included in the result.
+
+        :return list:
+            Menu items ordered from child to parent, unless ``revert=True``.
+        """
+        result = list()
+        menu = self if include_self else self.parent_id
+        while menu:
+            result.append(menu)
+            menu = menu.parent_id
+        return reversed(result) if revert else result
