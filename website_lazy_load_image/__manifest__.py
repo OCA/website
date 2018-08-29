3247d29de2944ccb9c436a8f33a526a165d7d19a
tarteo <d.sluijk@onestein.nl>
2018-08-29 11:03:57 +0200

    [ADD] website_lazy_load_image
    [FIX] Lint
    
    [IMP] img tranformation in ir.ui.view
    
    [FIX] jsdocs and flake8
    
    [ADD] Excluding images and more documentation
    
    [IMP] Better usage text
    
    [FIX] Flake and jslint
    
    [ADD] Tests
    
    [FIX] missing EOF new line
    
    [IMP] Use Savepointcase for tests
    
    [IMP] Use Savepointcase for tests

---
+++ website_lazy_load_image/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2018 Onestein
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Website Lazy Load Images',
+    'summary': 'Load images only when they are visible in the viewport.',
+    'author': 'Onestein, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/oca/website',
+    'category': 'Website',
+    'version': '11.0.1.0.0',
+    'depends': [
+        'website'
+    ],
+    'data': [
+        'templates/assets.xml',
+        'templates/snippets.xml'
+    ],
+}
