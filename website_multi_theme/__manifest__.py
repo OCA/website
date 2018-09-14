bcd37269121acaabf32ee3dfd8432d832a79e93d
Ivan Yelizariev <yelizariev@it-projects.info>
2018-09-14 11:05:38 +0500

    [DOC] bump 11.0.1.6.0"

--- website_multi_theme/__manifest__.py
+++ website_multi_theme/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Website Multi Theme",
     "summary": "Support different theme per website",
-    "version": "11.0.1.5.0",
+    "version": "11.0.1.6.0",
     "category": "Website",
     "website": "https://www.tecnativa.com",
     "author": "Tecnativa, IT-Projects LLC, Onestein, "
