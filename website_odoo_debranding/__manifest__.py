d79a8ad17a09b120172ca61d715ec669bece7ced
dreambits <admin@dreambits.in>
2018-10-01 15:20:54 +0530

    [MIG] website_odoo_debranding: Migration to 12.0

--- website_odoo_debranding/__manifest__.py
+++ website_odoo_debranding/__manifest__.py
@@ -1,9 +1,8 @@
 # Copyright 2016 Tecnativa, S.L. - Vicent Cubells
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-
 {
-    "name": "Remove odoo.com bindings on website",
-    "version": "11.0.1.0.0",
+    "name": "Remove Odoo Branding from Website",
+    "version": "12.0.1.0.0",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/website",
