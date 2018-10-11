9afe804754ed1c7a650810652235956463d9dee4
Cris Martín <cristina.marrod@gmail.com>
2018-10-11 13:40:02 +0200

    [11.0][MIG] website_google_tag_manager: Migration to 11.0

--- website_google_tag_manager/models/website.py
+++ website_google_tag_manager/models/website.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 ABF OSIELL <http://osiell.com>
+# Copyright 2018 Tecnativa - Cristina Martin R.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields
+from odoo import fields, models
 
 
 class Website(models.Model):
     _inherit = 'website'
 
-    google_tag_manager_key = fields.Char(u"Google Tag Manager Key")
+    google_tag_manager_key = fields.Char("Container ID")
