a52d28ebd9066dfadb75444d2a9398c27772ccdd
Cris Martín <cristina.marrod@gmail.com>
2018-10-11 13:40:02 +0200

    [11.0][MIG] website_google_tag_manager: Migration to 11.0

--- website_google_tag_manager/models/__init__.py
+++ website_google_tag_manager/models/__init__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import website
-from . import website_config_settings
+from . import res_config_settings
---
+++ website_google_tag_manager/readme/CONTRIBUTORS.rst
@@ -0,0 +1,5 @@
+* `Sebastien Alix <sebastien.alix@osiell.com>`_
+
+* `Tecnativa <https://www.tecnativa.com>`__:
+
+  * Cristina Martin R.
