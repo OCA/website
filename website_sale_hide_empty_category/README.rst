c9de15edd76e3898c024c7928b0d2da125f8618d
Rémy Taymans <remytaymans@gmail.com>
2018-10-27 09:00:08 +0200

    [MIG] website_sale_hide_empty_category: Migration to 11.0

--- website_sale_hide_empty_category/README.rst
+++ website_sale_hide_empty_category/README.rst
@@ -17,7 +17,7 @@ categories disappear
 
 .. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
    :alt: Try me on Runbot
-   :target: https://runbot.odoo-community.org/runbot/186/10.0
+   :target: https://runbot.odoo-community.org/runbot/186/11.0
 
 Bug Tracker
 ===========
--- website_sale_hide_empty_category/__manifest__.py
+++ website_sale_hide_empty_category/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 LasLabs Inc.
+# Copyright 2017 LasLabs Inc. <https://laslabs.com/>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Website Sale - Hide Empty Categories",
     "summary": "Hide any Product Categories that are empty",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Website",
-    "website": "https://laslabs.com/",
+    "website": "https://github.com/OCA/website/",
     "author": "LasLabs, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "application": False,
