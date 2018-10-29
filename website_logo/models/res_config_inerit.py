c926375b4d90e6135125b5cf4b8e2579db529b25
eslam <e.amer@centione.com>
2018-10-29 03:51:42 +0200

    [MIG]  : Migration to 12.0

--- website_logo/models/res_config_inerit.py
+++ website_logo/models/res_config_inerit.py
@@ -1,4 +1,6 @@
 from odoo import fields, models
+
+
 class Config(models.TransientModel):
     _inherit = 'res.config.settings'
     website_logo = fields.Binary(related='website_id.website_logo',
--- website_logo/models/website_inherit.py
+++ website_logo/models/website_inherit.py
@@ -1,4 +1,6 @@
 from odoo import fields, models
+
+
 class Website(models.Model):
     _inherit = 'website'
     website_logo = fields.Binary()
