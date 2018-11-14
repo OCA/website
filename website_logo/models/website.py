7cb9702cf81fbbc330fefc542e439039013a82f1
eslam <e.amer@centione.com>
2018-11-14 21:16:41 +0200

    [IMP] website_logo: remove whitespaces

--- website_logo/models/website.py
+++ website_logo/models/website.py
@@ -1,9 +1,9 @@
-from odoo import fields, models
 # Copyright 2015 Agile Business Group - Lorenzo Battistini
 # Copyright 2016 Tecnativa - Antonio Espinosa
 # Copyright 2017 Tecnativa - David Vidal
 # Copyright 2018 eslaAmer - Eslam Amer
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
 
 
 class Website(models.Model):
