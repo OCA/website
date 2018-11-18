5c5b1a482c3b7bb4e4a8bdcb5fbfbfb84f200f11
ernesto <ernesto.tejeda87@gmail.com>
2018-11-17 19:57:19 -0500

    [MIG] website_crm_privacy_policy: Migration to 12.0

--- website_crm_privacy_policy/__manifest__.py
+++ website_crm_privacy_policy/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014 Jorge Camacho <jcamacho@trey.es>
 # Copyright 2015 Antonio Espinosa <antonioea@antiun.com>
 # Copyright 2016-2018 Vicent Cubells <vicent.cubells@tecnativa.com>
@@ -6,18 +5,11 @@
 {
     "name": "Website CRM privacy policy",
     "category": "Website",
-    "version": "11.0.1.0.0",
+    "version": "13.0.1.0.0",
     "website": "https://github.com/OCA/website",
-    "author": "Trey, "
-              "Tecnativa, "
-              "Odoo Community Association (OCA)",
+    "author": "Trey, " "Tecnativa, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
-    "depends": [
-        "website_crm",
-        "website_legal_page",
-    ],
-    "data": [
-        "templates/contactus.xml",
-    ],
+    "depends": ["website_crm", "website_legal_page"],
+    "data": ["templates/contactus.xml"],
 }
