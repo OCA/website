39a2c8733355cdc0b1e798cff361481ef43db1cf
Simone Orsi <simahawk@gmail.com>
2019-01-09 17:52:42 +0100

    [REF] website_form_recaptcha: better API, allow per-website config
    * API keys can now be configured via website settings
      which in turn allow to customize the values per website
      in a multi-website instance;
    
    * move all internal APIs to the model `website.form.recaptcha`
      so that we do not depend anymore on the controller
      if we need to integrate it into other pieces of code.

--- website_form_recaptcha/controllers/main.py
+++ website_form_recaptcha/controllers/main.py
@@ -1,9 +1,9 @@
 # Copyright 2016-2017 LasLabs Inc.
+# Copyright 2019 Simone Orsi - Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import http
 from odoo.http import request
-from odoo.exceptions import ValidationError
 
 from odoo.addons.website_form.controllers.main import WebsiteForm
 
@@ -21,38 +21,18 @@ class WebsiteForm(WebsiteForm):
         multilang=False,
     )
     def recaptcha_public(self):
+        recaptcha_model = request.env['website.form.recaptcha'].sudo()
+        creds = recaptcha_model._get_api_credentials(
+            request.website,
+        )
         return json.dumps({
-            'site_key': request.env['ir.config_parameter'].sudo().get_param(
-                'recaptcha.key.site'
-            ),
+            'site_key': creds['site_key']
         })
 
     def extract_data(self, model, values):
         """ Inject ReCaptcha validation into pre-existing data extraction """
         res = super(WebsiteForm, self).extract_data(model, values)
         if model.website_form_recaptcha:
-            captcha_obj = request.env['website.form.recaptcha']
-            # Only check once: if a call to reCAPTCHA's API is made twice with
-            # the same token, we get a 'timeout-or-duplicate' error. So we
-            # stick to the first response data storing the token after the
-            # first invoke in the current request object. This duplicated
-            # call can be cause for instance by website_crm_phone_validation.
-            try:
-                getattr(request, captcha_obj.RESPONSE_ATTR)
-            except AttributeError:
-                return res
-            ip_addr = request.httprequest.environ.get('HTTP_X_FORWARDED_FOR')
-            if ip_addr:
-                ip_addr = ip_addr.split(',')[0]
-            else:
-                ip_addr = request.httprequest.remote_addr
-            try:
-                captcha_obj.action_validate(
-                    values.get(captcha_obj.RESPONSE_ATTR), ip_addr
-                )
-                # Store reCAPTCHA's token in the current request object
-                setattr(request, captcha_obj.RESPONSE_ATTR,
-                        values.get(captcha_obj.RESPONSE_ATTR))
-            except ValidationError:
-                raise ValidationError([captcha_obj.RESPONSE_ATTR])
+            recaptcha_model = request.env['website.form.recaptcha'].sudo()
+            recaptcha_model.validate_request(request, values)
         return res
