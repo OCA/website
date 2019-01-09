5a91a42cabbb00ad32feb719840112ac4fea8046
Simone Orsi <simahawk@gmail.com>
2019-01-09 17:52:42 +0100

    [REF] website_form_recaptcha: better API, allow per-website config
    * API keys can now be configured via website settings
      which in turn allow to customize the values per website
      in a multi-website instance;
    
    * move all internal APIs to the model `website.form.recaptcha`
      so that we do not depend anymore on the controller
      if we need to integrate it into other pieces of code.

--- website_form_recaptcha/models/website_form_recaptcha.py
+++ website_form_recaptcha/models/website_form_recaptcha.py
@@ -1,7 +1,8 @@
 # Copyright 2015-2017 LasLabs Inc.
+# Copyright 2019 Simone Orsi - Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, api, _
+from odoo import models, api, http, _
 from odoo.exceptions import ValidationError
 import requests
 
@@ -29,8 +30,27 @@ class WebsiteFormRecaptcha(models.AbstractModel):
         return map.get(errorcode, _('There was a problem with '
                                     'the captcha entry.'))
 
+    def _get_api_params(self):
+        ICP = self.env['ir.config_parameter'].sudo()
+        return {
+            'site_key': ICP.get_param('recaptcha.key.site'),
+            'secret_key': ICP.get_param('recaptcha.key.secret'),
+        }
+
+    def _get_api_credentials(self, website=None):
+        # defaults
+        params = self._get_api_params()
+        # website override
+        website = website or http.request.website
+        site_key = website.recaptcha_key_site or params['site_key']
+        secret_key = website.recaptcha_key_secret or params['secret_key']
+        return {
+            'site_key': site_key,
+            'secret_key': secret_key,
+        }
+
     @api.model
-    def action_validate(self, response, remote_ip):
+    def validate_response(self, response, remote_ip, website=None):
         """ Validate ReCaptcha Response
         Params:
             response: str The value of 'g-recaptcha-response'.
@@ -41,16 +61,13 @@ class WebsiteFormRecaptcha(models.AbstractModel):
             True on success
         """
 
-        ICP = self.env['ir.config_parameter'].sudo()
-        secret_key = ICP.get_param('recaptcha.key.secret')
-
         # @TODO: Domain validation
         # domain_name = request.httprequest.environ.get(
         #     'HTTP_HOST', ''
         # ).split(':')[0]
-
+        creds = self._get_api_credentials(website=website)
         data = {
-            'secret': secret_key,
+            'secret': creds['secret_key'],
             'response': response,
             'remoteip': remote_ip,
         }
@@ -65,3 +82,30 @@ class WebsiteFormRecaptcha(models.AbstractModel):
             raise ValidationError(self._get_error_message())
 
         return True
+
+    @api.model
+    def validate_request(self, request, values):
+        # Only check once: if a call to reCAPTCHA's API is made twice with
+        # the same token, we get a 'timeout-or-duplicate' error. So we
+        # stick to the first response data storing the token after the
+        # first invoke in the current request object. This duplicated
+        # call can be cause for instance by website_crm_phone_validation.
+        try:
+            getattr(request, self.RESPONSE_ATTR)
+        except AttributeError:
+            return None
+        ip_addr = request.httprequest.environ.get('HTTP_X_FORWARDED_FOR')
+        if ip_addr:
+            ip_addr = ip_addr.split(',')[0]
+        else:
+            ip_addr = request.httprequest.remote_addr
+        try:
+            self.validate_response(
+                values.get(self.RESPONSE_ATTR), ip_addr
+            )
+            # Store reCAPTCHA's token in the current request object
+            setattr(
+                request, self.RESPONSE_ATTR, values.get(self.RESPONSE_ATTR)
+            )
+        except ValidationError:
+            raise ValidationError([self.RESPONSE_ATTR])
