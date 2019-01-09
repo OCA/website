f46d879acf33b4c68843f4887f29e056ec622973
Simone Orsi <simahawk@gmail.com>
2019-01-09 17:52:42 +0100

    [REF] website_form_recaptcha: better API, allow per-website config
    * API keys can now be configured via website settings
      which in turn allow to customize the values per website
      in a multi-website instance;
    
    * move all internal APIs to the model `website.form.recaptcha`
      so that we do not depend anymore on the controller
      if we need to integrate it into other pieces of code.

--- website_form_recaptcha/__manifest__.py
+++ website_form_recaptcha/__manifest__.py
@@ -4,12 +4,11 @@
 {
     "name": "Website Form - ReCaptcha",
     "summary": 'Provides a ReCaptcha field for Website Forms',
-    "version": "11.0.1.1.0",
+    "version": "11.0.1.2.0",
     "category": "Website",
     "website": "https://github.com/OCA/website",
     "author": "LasLabs, Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "application": False,
     'installable': True,
     "depends": [
         "website_form",
@@ -17,5 +16,7 @@
     "data": [
         "data/ir_config_parameter_data.xml",
         'views/assets.xml',
+        'views/website_config_settings.xml',
+        'views/website.xml',
     ],
 }
