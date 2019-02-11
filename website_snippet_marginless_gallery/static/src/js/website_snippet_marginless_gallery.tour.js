5c6b16c4e24a0d3799cc552540950ac1eedd3cf4
Cristina Martin <cristina.martin@tecnativa.com>
2019-02-11 13:31:53 +0100

    [12.0][MIG] website_snippet_marginless_gallery: Migration to v12

--- website_snippet_marginless_gallery/static/src/js/website_snippet_marginless_gallery.tour.js
+++ website_snippet_marginless_gallery/static/src/js/website_snippet_marginless_gallery.tour.js
@@ -1,74 +1,27 @@
-/* Copyright 2015-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
+/* Copyright 2015-2017 Tecnativa - Jairo Llopis <jairo.llopis@tecnativa.com>
+ * Copyright 2019 Tecnativa - Cristina Martin R.
  * License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html). */
 
-odoo.define("web_module_name.tour", function (require) {
-    "use strict";
-    var base = require("web_editor.base");
-    var Tour = require("web.Tour");
+odoo.define('website_snippet_marginless_gallery.tour', function (require) {
+    'use strict';
+    var core = require('web.core');
+    var tour = require('web_tour.tour');
+    var base = require('web_editor.base');
 
-    return base.ready().done($.proxy(Tour, "register", {
-        id: "marginless_gallery",
-        name: "Insert a marginless gallery snippet",
-        path: "/",
-        mode: "test",
-        steps: [
-            {
-                title: "Edit the homepage (1)",
-                waitFor: "button[data-action=edit]",
-                element: "button[data-action=edit]",
-            },
-            {
-                title: "Drag and drop a marginless gallery snippet (1)",
-                waitFor: ".oe_snippet:contains('Marginless Gallery'):visible",
-                snippet: ".oe_snippet:contains('Marginless Gallery')",
-            },
-            {
-                title: "Cancel modal dialog",
-                waitFor: ".modal button:contains('Cancel'):visible",
-                element: ".modal button:contains('Cancel'):visible",
-            },
-            {
-                title: "Drag and drop a marginless gallery snippet (2)",
-                waitFor: ".oe_snippet:contains('Marginless Gallery'):visible",
-                snippet: ".oe_snippet:contains('Marginless Gallery')",
-            },
-            {
-                title: "Set no fixed height",
-                waitFor: ".modal .btn-primary:visible",
-                element: ".modal .btn-primary:visible",
-            },
-            {
-                title: "Click on the snippet to see options",
-                waitNot: ".modal:visible",
-                element: "#wrap .marginless-gallery",
-            },
-            {
-                title: "Click on Customize",
-                waitFor: "#wrap .marginless-gallery, \
-                          .btn-primary:contains('Customize'):visible",
-                element: ".btn-primary:contains('Customize'):visible",
-            },
-            {
-                title: "Click on Change Images Height",
-                element: "li[data-change_images_height]:visible a",
-            },
-            {
-                title: "Set image height of 200px",
-                waitFor: ".modal-body input:visible",
-                element: ".modal-body input:visible",
-                sampleText: "200",
-            },
-            {
-                title: "Accept modal dialog",
-                waitFor: ".modal input:contains('200'):visible",
-                element: ".modal .btn-primary:visible",
-            },
-            {
-                title: "Check that the right height has been saved",
-                waitNot: ".modal:visible",
-                waitFor: "#wrap .marginless-gallery \
-                          .col-md-3[style*='height: 200px']",
-            },
-        ],
-    }));
+    var _t = core._t;
+
+    tour.register("marginless_gallery", {
+        url: "/",
+        wait_for: base.ready(),
+    }, [{
+        trigger: "a[data-action=edit]",
+        content: _t("<b>Click Edit</b> to start designing your homepage."),
+        extra_trigger: ".homepage",
+        position: "bottom",
+    }, {
+        trigger: "#snippet_feature .oe_snippet:contains('Marginless Gallery')",
+        content: _t("Drag <i>Marginless Gallery</i> and drop it on the page."),
+        position: "bottom",
+        run: "drag_and_drop #wrap",
+    }]);
 });
