6d725b82d29a461c0f3227c64ff632b1f7348978
Cristina Martin <cristina.martin@tecnativa.com>
2019-02-11 13:31:53 +0100

    [12.0][MIG] website_snippet_marginless_gallery: Migration to v12

---
+++ website_snippet_marginless_gallery/readme/USAGE.rst
@@ -0,0 +1,12 @@
+To use this module, you need to:
+
+* Edit any page in your website.
+* Go to **Insert blocks > Features**.
+* Drag the **Marginless Gallery** snippet anywhere in your page.
+
+To change images, you need to:
+
+* Select any image block in the snippet you just created.
+* Go to **Customize > Background** and choose any background you want.
+
+* Backgrounds will be centered and cover the whole element in any layout.
--- website_snippet_marginless_gallery/tests/test_ui.py
+++ website_snippet_marginless_gallery/tests/test_ui.py
@@ -1,14 +1,16 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2015-2017 Tecnativa - Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2019 Tecnativa - Cristina Martin R.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from openerp.tests.common import HttpCase
+import odoo.tests
 
 
-class UICase(HttpCase):
+class UICase(odoo.tests.HttpCase):
     def test_admin_tour_marginless_gallery(self):
-        self.phantom_js(
+        self.browser_js(
             "/",
-            "odoo.__DEBUG__.services['web.Tour'].run('marginless_gallery')",
-            "odoo.__DEBUG__.services['web.Tour'].tours.marginless_gallery",
-            "admin")
+            "odoo.__DEBUG__.services['web_tour.tour']"
+            ".run('marginless_gallery')",
+            "odoo.__DEBUG__.services['web_tour.tour']"
+            ".tours.marginless_gallery.ready",
+            login="admin")
