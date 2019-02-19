579d20845c0b410f1894a8ec8137a5fd1a4e081a
Cristina Martin <cristina.martin@tecnativa.com>
2019-02-19 13:56:44 +0100

    [12.0][MIG] website_snippet_big_button: Migration to v12

--- website_snippet_big_button/__manifest__.py
+++ website_snippet_big_button/__manifest__.py
@@ -1,12 +1,12 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015-2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2015-2016 Tecnativa - Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2019 Tecnativa - Cristina Martin R.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Big Buttons Snippet",
     "summary": "A snippet that adds two big buttons",
-    "version": "9.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Website",
-    "website": "https://www.tecnativa.com/",
+    "website": "https://github.com/OCA/website",
     "author": "Grupo ESOC Ingeniería de Servicios, S.L.U., "
               "Tecnativa, "
               "Odoo Community Association (OCA)",
---
+++ website_snippet_big_button/readme/USAGE.rst
@@ -0,0 +1,7 @@
+To use this module, you need to:
+
+* Edit any web page.
+* Go to **Insert Blocks > Big Buttons**.
+* Drag it anywhere.
+
+You can edit the link anywhere on the button.
