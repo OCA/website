689329836a893d4f8a46116d8fa7bbcafe1f1c84
Cristina Martin <cristina.martin@tecnativa.com>
2019-02-25 13:41:08 +0100

    [12.0][MIG] website_form_captcha: Migration to v12

--- website_form_recaptcha/controllers/__init__.py
+++ website_form_recaptcha/controllers/__init__.py
@@ -1,4 +1,3 @@
-# Copyright 2016-2017 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import main
--- website_form_recaptcha/models/__init__.py
+++ website_form_recaptcha/models/__init__.py
@@ -1,4 +1,3 @@
-# Copyright 2015-2017 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import ir_model
