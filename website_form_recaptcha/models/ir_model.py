c80bf305af17ab4822acca670568bb9bfa3371b5
Cristina Martin <cristina.martin@tecnativa.com>
2019-02-25 13:41:08 +0100

    [12.0][MIG] website_form_captcha: Migration to v12

--- website_form_recaptcha/models/ir_model.py
+++ website_form_recaptcha/models/ir_model.py
@@ -1,7 +1,7 @@
 # Copyright 2015-2017 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class IrModel(models.Model):
