440416fea240a03f61bc18b4d20781798664b692
Cristina Martin <cristina.martin@tecnativa.com>
2019-02-27 19:50:47 +0100

    [12.0][MIG] website_crm_quick_answer: Migration to v12

--- website_crm_quick_answer/__manifest__.py
+++ website_crm_quick_answer/__manifest__.py
@@ -1,21 +1,23 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Tecnativa - Jairo Llopis
 # Copyright 2017 Tecnativa - David Vidal
+# Copyright 2019 Tecnativa - Cristina Martin R.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': "Quick answer for website contact form",
     "summary": "Add an automatic answer for contacts asking for info",
-    'category': 'Customer Relationship Management',
-    'version': '10.0.1.0.0',
+    'category': 'Website',
+    'version': '12.0.1.0.0',
+    'website': 'https://github.com/OCA/website',
     'depends': [
         'website_crm',
+        'base_automation',
     ],
     'data': [
-        "data/automation.xml",
+        "data/base_automation_data.xml",
     ],
     'author': 'Tecnativa, '
               'Odoo Community Association (OCA)',
-    'website': 'https://www.tecnativa.com',
     'license': 'AGPL-3',
+    'application': False,
     'installable': True,
 }
