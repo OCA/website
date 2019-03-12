55c5cf4849ddcee8529d1f98780196e41dd96d8e
cristinamartinrod <cristina.martin@tecnativa.com>
2019-03-12 10:35:18 +0100

    [12.0][MIG] website_breadcrumb: Migration to v12

--- website_breadcrumb/models/website.py
+++ website_breadcrumb/models/website.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# Copyright 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from odoo import api, models
