f9257cc4f2b745a660d6b68deaff3773648c0471
cristinamartinrod <cristina.martin@tecnativa.com>
2019-03-12 10:35:18 +0100

    [12.0][MIG] website_breadcrumb: Migration to v12

--- website_breadcrumb/__manifest__.py
+++ website_breadcrumb/__manifest__.py
@@ -1,12 +1,12 @@
-# -*- coding: utf-8 -*-
-# © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# Copyright 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# Copyright 2019 Tecnativa - Cristina Martin R.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Website Breadcrumbs",
     "summary": "Let you have breadcrumbs in website pages",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Website",
-    "website": "https://grupoesoc.es/",
+    "website": "https://github.com/OCA/website",
     "author": "Grupo ESOC Ingeniería de Servicios, "
               "Odoo Community Association (OCA)",
     "license": "LGPL-3",
