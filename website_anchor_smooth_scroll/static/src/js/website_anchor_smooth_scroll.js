f720c4a7fe8a9378dd2a4009a58652c26ee7ee50
tbaden <thore.baden@gmx.net>
2019-05-09 00:28:16 +0200

    [FIX][11.0] website_anchor_smooth_scroll travis warn

--- website_anchor_smooth_scroll/static/src/js/website_anchor_smooth_scroll.js
+++ website_anchor_smooth_scroll/static/src/js/website_anchor_smooth_scroll.js
@@ -2,25 +2,25 @@
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
 odoo.define('website_anchor_smooth_scroll.website_anchor_smooth_scroll', function (require) {
 
-"use strict";
+    "use strict";
 
-function website_anchor_smooth_scroll(event) {
-    event.preventDefault();
-    var target = $(event.currentTarget.hash);
+    function website_anchor_smooth_scroll (event) {
+        event.preventDefault();
+        var target = $(event.currentTarget.hash);
 
-    return $('html, body')
-    .stop()
-    .animate({
-        'scrollTop': target.offset().top - 100
-    })
-    .promise()
-    .done(function(element) {
-        history.pushState(null, document.title, event.target.hash);
-    });
-}
+        return $('html, body')
+            .stop()
+            .animate({
+                'scrollTop': target.offset().top - 100,
+            })
+            .promise()
+            .done(function () {
+                history.pushState(null, document.title, event.target.hash);
+            });
+    }
 
-require('web.dom_ready');
+    require('web.dom_ready');
 
-$("a[href^='#']:not([href=#])").on("click", website_anchor_smooth_scroll);
+    $("a[href^='#']:not([href=#])").on("click", website_anchor_smooth_scroll);
 
 });
