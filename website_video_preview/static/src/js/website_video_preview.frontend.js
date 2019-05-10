fb1c1298a7e8770875b79e67662479878a8178e7
tbaden <thore.baden@gmx.net>
2019-05-10 22:55:01 +0200

    [FIX][11.0] website_video_preview travis warn

--- website_video_preview/static/src/js/website_video_preview.frontend.js
+++ website_video_preview/static/src/js/website_video_preview.frontend.js
@@ -1,4 +1,4 @@
-odoo.define('website_video_preview.frontend', function(require) {
+odoo.define('website_video_preview.frontend', function (require) {
     "use strict";
 
     var registry = require('website.content.snippets.animation').registry;
@@ -9,42 +9,48 @@ odoo.define('website_video_preview.frontend', function(require) {
             'click .play_button': '_onPlayButtonClick',
         },
 
-        start: function() {
-            var src = this.$target.find('img.video_preview').attr('data-iframe-src');
+        start: function () {
+            var src = this.$target
+                .find('img.video_preview').attr('data-iframe-src');
             if (!src) {
                 return this._super.apply(this, arguments);
             }
             if (this.editableMode) {
                 this.stopVideo();
-            } else if(src.indexOf('autoplay=1') !== -1) {
-                this.playVideo(this.$target.find('img.video_preview').attr('data-iframe-src'));
+            } else if (src.indexOf('autoplay=1') !== -1) {
+                this.playVideo(
+                    this.$target
+                        .find('img.video_preview').attr('data-iframe-src'));
             }
         },
 
-        _onPlayButtonClick: function(e) {
+        _onPlayButtonClick: function (e) {
             var $img = $(e.currentTarget);
-            var src = $img.parent().find('[data-iframe-src]').attr('data-iframe-src');
+            var src = $img.parent()
+                .find('[data-iframe-src]').attr('data-iframe-src');
             this.playVideo(src);
         },
 
-        _onPreviewClick: function(e) {
+        _onPreviewClick: function (e) {
             var $img = $(e.currentTarget);
             var src = $img.attr('data-iframe-src');
             this.playVideo(src);
         },
 
-        playVideo: function(src) {
-            var $iframe = $('<iframe class="o_video_dialog_iframe" frameborder="0"></iframe>');
+        playVideo: function (src) {
+            var $iframe = $(
+                "<iframe/>",
+                {"class": "o_video_dialog_iframe", "frameborder": 0});
             $iframe.attr('src', src.replace('autoplay=0', 'autoplay=1'));
             this.$target.append($iframe);
             this.$target.find('img.video_preview').addClass('hidden');
             this.$target.find('img.play_button').addClass('hidden');
         },
 
-        stopVideo: function() {
+        stopVideo: function () {
             this.$target.find('iframe').remove();
             this.$target.find('img.video_preview').removeClass('hidden');
             this.$target.find('img.play_button').removeClass('hidden');
-        }
+        },
     });
 });
