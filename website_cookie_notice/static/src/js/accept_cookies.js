b08312bd6a638cc1e0c412e953e48ac5bd912794
tbaden <thore.baden@gmx.net>
2019-05-11 18:54:22 +0200

    [FIX][10.0] website_cookie_notice travis warn

--- website_cookie_notice/static/src/js/accept_cookies.js
+++ website_cookie_notice/static/src/js/accept_cookies.js
@@ -8,10 +8,10 @@ odoo.define('website_cookie_notice.cookie_notice', function (require) {
 
     var base = require('web_editor.base');
 
-    base.ready().done(function() {
-        $(".cc-cookies .btn-primary").click(function(e) {
+    base.ready().done(function () {
+        $(".cc-cookies .btn-primary").click(function (e) {
             e.preventDefault();
-            // max-age to store cookie for one year.
+            // Max-age to store cookie for one year.
             document.cookie = 'accepted_cookies=1; path=/; max-age=31536000';
             $(e.target).closest(".cc-cookies").hide("fast");
         });
