43877b79e8c23e9a92c06c0c88f604ae80052919
tbaden <thore.baden@gmx.net>
2019-05-29 22:03:31 +0200

    [10.0][FIX] website_anchor_smooth_scroll travis warn

--- website_anchor_smooth_scroll/static/src/js/website_anchor_smooth_scroll.js
+++ website_anchor_smooth_scroll/static/src/js/website_anchor_smooth_scroll.js
@@ -4,25 +4,27 @@
 *    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 **/
 
-odoo.define('website_anchor_smooth_scroll', function(require) {
+odoo.define('website_anchor_smooth_scroll', function (require) {
     'use strict';
 
     var base = require('web_editor.base');
 
-    var smooth_scroll = function(event) {
+    var smooth_scroll = function (event) {
         event.preventDefault();
         var anchor_fragment = event.target.hash;
 
-        // Do this before scrolling so that browser history accurately reflects scroll position at time of click 
+        // Do this before scrolling so that browser history
+        // accurately reflects scroll position at time of click
         history.pushState(null, document.title, anchor_fragment);
 
         return $('html, body').stop().animate({
-            'scrollTop': $(anchor_fragment).offset().top - 100
+            'scrollTop': $(anchor_fragment).offset().top - 100,
         }).promise();
     };
 
-    base.ready().done(function() {
-        $('a[href^="#"][href!="#"][href!="#advanced-view-editor"]').click(smooth_scroll);
+    base.ready().done(function () {
+        $('a[href^="#"][href!="#"][href!="#advanced-view-editor"]')
+            .click(smooth_scroll);
     });
 
     return {'scroll_handler': smooth_scroll};
