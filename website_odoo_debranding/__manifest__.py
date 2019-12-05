852dd8956be95bcc22e831d0e9e3a78145c2baae
ougc27 <ougc27@gmail.com>
2019-12-05 14:27:31 -0600

    [MIG] website_odoo_debranding: Migration to 13.0

--- website_odoo_debranding/__manifest__.py
+++ website_odoo_debranding/__manifest__.py
@@ -2,17 +2,13 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Remove Odoo Branding from Website",
-    "version": "12.0.1.0.0",
-    "author": "Tecnativa, "
-              "Odoo Community Association (OCA)",
+    "version": "13.0.1.0.0",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/website",
     "license": "LGPL-3",
     "category": "Website",
-    "depends": [
-        'website',
-    ],
-    "data": [
-        "templates/disable_odoo.xml",
-    ],
+    "depends": ["website"],
+    "data": ["templates/disable_odoo.xml"],
     "installable": True,
+    "post_init_hook": "post_init_hook",
 }
