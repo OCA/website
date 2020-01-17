150047a9733710d4e82eb4fdb33a21101975c6f7
tarteo <d.sluijk@onestein.nl>
2020-01-17 17:07:35 +0100

    [IMP] website_snippet_preset: black, isort

--- website_snippet_preset/models/snippet_preset.py
+++ website_snippet_preset/models/snippet_preset.py
@@ -1,11 +1,11 @@
 # Copyright 2018 Onestein
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class SnippetPreset(models.Model):
-    _name = 'snippet.preset'
+    _name = "snippet.preset"
 
     snippet = fields.Char()
     name = fields.Char()
