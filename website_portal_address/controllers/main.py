9f9612247ba52c004f7154e01ed5ddb1ec7e7f9b
Mathieu Benoit <mathieu.benoit@mathben.tech>
2020-03-08 22:01:24 -0400

    [FIX] website_portal_address: filter state by country - fix hidden address when choose contact

--- website_portal_address/controllers/main.py
+++ website_portal_address/controllers/main.py
@@ -1,26 +1,26 @@
 # Copyright 2016-Today Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
+import logging
+
 from odoo.http import request, route
-from odoo.addons.website_portal_contact.controllers.main import (
-    WebsiteAccount as PortalController,
-)
+from odoo.addons.website_portal_contact.controllers.main import WebsiteAccount
+
+_logger = logging.getLogger(__name__)
 
 
-class WebsiteAccount(PortalController):
+class WebsiteAccount(WebsiteAccount):
     @route(
         "/my/contacts/<model('res.partner'):contact>",
         auth="user",
         website=True,
     )
-    def portal_my_contacts_read(self, contact):
-        values = self._prepare_portal_layout_values()
+    def portal_my_contacts_read(self, contact, access_token=None, **kw):
+        values = self._contact_get_page_view_values(contact, access_token, **kw)
         values.update(
             {
                 "state_id": request.env["res.country.state"].sudo().search([]),
                 "country_id": request.env["res.country"].sudo().search([]),
-                "contact": contact,
-                "fields": self._contacts_fields(),
             }
         )
         return request.render(
@@ -39,3 +39,29 @@ class WebsiteAccount(PortalController):
             "state_id",
         ]
         return res
+
+    def _contacts_clean_values(self, values, contact=False):
+        """Set values to a write-compatible format"""
+        if "state_id" not in values:
+            # Force erase the data
+            values["state_id"] = False
+        elif values["state_id"]:
+            try:
+                values["state_id"] = int(values["state_id"].split('-', 1)[1])
+            except ValueError:
+                _logger.warning('Cannot parse "state_id" : %s' % (values["state_id"]))
+
+        if "country_id" in values and values["country_id"]:
+            try:
+                values["country_id"] = int(values["country_id"])
+            except ValueError:
+                _logger.warning(
+                    'Cannot parse "country_id" : %s' % (values["country_id"]))
+
+        result = {k: v or False for k, v in values.items()}
+        result.setdefault("type", "contact")
+        if not contact or contact.id != request.env.user.commercial_partner_id.id:
+            result.setdefault(
+                "parent_id", request.env.user.commercial_partner_id.id
+            )
+        return result
