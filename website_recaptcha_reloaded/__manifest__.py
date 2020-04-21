ae43156254c7e2ae994ae964e511e9a59a28a005
robin.keunen <robin@keunen.net>
2020-04-21 18:22:16 +0200

    [REF] OCA pre-commit compliance

--- website_recaptcha_reloaded/__manifest__.py
+++ website_recaptcha_reloaded/__manifest__.py
@@ -1,26 +1,14 @@
 # Copyright 2004 Tech-Receptives Solutions Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Website reCAPTCHA Reloaded',
-    'version': '12.0.0.1',
-    'category': 'Website',
-    'depends': ['website'],
-    'author': 'Tech Receptives',
-    'license': 'AGPL-3',
-    'website': 'https://www.techreceptives.com',
-    'description': """
-    Odoo Website reCAPTCHA Reloaded
-    ================================
-    This modules allows you to integrate Google reCAPTCHA v2.0 to your website
-    forms. You can configure your Google reCAPTCHA site and public keys
-    in "Settings" -> "Website Settings"
-
-    You will need to install various website_<module>_recaptcha modules
-    to use it in your various pages.
-    """,
-    'data': [
-      'views/website_view.xml',
-      'views/res_config.xml',
-     ],
-    'installable': True,
+    "name": "Website reCAPTCHA Reloaded",
+    "version": "12.0.0.0.1",
+    "category": "Website",
+    "depends": ["website"],
+    "author": "Tech Receptives",
+    "license": "AGPL-3",
+    "website": "https://www.techreceptives.com",
+    "summary": "Add google recaptcha to forms.",
+    "data": ["views/website_view.xml", "views/res_config.xml"],
+    "installable": True,
 }
