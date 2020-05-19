560ead8330e85aece16d91b65371ad95b23e5eca
Miguel Poyatos <miguel@trey.es>
2020-05-19 12:42:00 +0200

    [13.0][ADD] website_fancybox

---
+++ website_fancybox/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Trey - Miguel Poyatos <miguel@trey.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Website Fancybox",
+    "category": "Website",
+    "summary": "Add scripts and styles to use Fancybox lightbox library",
+    "version": "13.0.1.0.0",
+    "author": "Trey, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/website",
+    "license": "AGPL-3",
+    "depends": ["website"],
+    "data": ["views/website_views.xml"],
+}
