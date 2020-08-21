d5246f3a9e8060c956b3d1a61cc0b3be3a413419
Harald Panten <harald.panten@sygel.es>
2020-08-21 10:29:35 +0200

    [IMP] website_crm_quick_answer: black, isort, prettier

--- website_crm_quick_answer/__manifest__.py
+++ website_crm_quick_answer/__manifest__.py
@@ -3,21 +3,15 @@
 # Copyright 2019 Tecnativa - Cristina Martin R.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': "Quick answer for website contact form",
+    "name": "Quick answer for website contact form",
     "summary": "Add an automatic answer for contacts asking for info",
-    'category': 'Website',
-    'version': '12.0.1.0.0',
-    'website': 'https://github.com/OCA/website',
-    'depends': [
-        'website_crm',
-        'base_automation',
-    ],
-    'data': [
-        "data/base_automation_data.xml",
-    ],
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'application': False,
-    'installable': True,
+    "category": "Website",
+    "version": "12.0.1.0.0",
+    "website": "https://github.com/OCA/website",
+    "depends": ["website_crm", "base_automation",],
+    "data": ["data/base_automation_data.xml",],
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
 }
