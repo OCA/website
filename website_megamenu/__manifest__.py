05e10bc10f2269e8be7c63952507a0577034d615
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-09-11 11:38:25 +0200

    [FIX] website_megamenu: Title s/wesbite/website

--- website_megamenu/__manifest__.py
+++ website_megamenu/__manifest__.py
@@ -2,7 +2,7 @@
 # Copyright 2020 Tecnativa - Alexandre Díaz
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    "name": "Wesbite Megamenu",
+    "name": "Website Megamenu",
     "category": "Menu/Megamenu",
     "summary": "Adds support for mega menus",
     "version": "12.0.1.0.0",
