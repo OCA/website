5827c38cede0103f3d34e1d34ff1a977a4c6247b
Ioan Galan <ioan@studio73.es>
2020-10-21 09:13:56 +0200

    [IMP] website_menu_by_user_status: black, isort, prettier

--- website_menu_by_user_status/tests/test_website_menu.py
+++ website_menu_by_user_status/tests/test_website_menu.py
@@ -5,19 +5,18 @@ from odoo.tests import SavepointCase
 
 
 class TestWebsiteMenu(SavepointCase):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.page = cls.env.ref('website.contactus_page')
+        cls.page = cls.env.ref("website.contactus_page")
         cls.page.is_visible = False
 
-        cls.menu = cls.env.ref('website.menu_contactus')
+        cls.menu = cls.env.ref("website.menu_contactus")
         cls.menu.user_logged = False
         cls.menu.user_not_logged = False
 
-        cls.public_user = cls.env.ref('base.public_user')
-        cls.demo_user = cls.env.ref('base.user_demo')
+        cls.public_user = cls.env.ref("base.public_user")
+        cls.demo_user = cls.env.ref("base.user_demo")
 
     def test_visible_user_logged_demo(self):
         self.page.is_visible = True
