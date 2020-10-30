d5309789958a7185b416e448b139dcfaf0449260
Ioan Galan <ioan@studio73.es>
2020-10-30 15:49:26 +0100

    [MIG] website_cookiebot: Migration to 12.0

--- website_cookiebot/__manifest__.py
+++ website_cookiebot/__manifest__.py
@@ -4,18 +4,17 @@
     'name': 'Website Cookiebot',
     'summary': 'Cookiebot integration',
     'category': 'Website',
-    'version': '13.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Trey (www.trey.es), Odoo Community Association (OCA)',
     'website': 'https://www.trey.es',
     'license': 'AGPL-3',
     'depends': [
-        'portal',
         'website',
     ],
     'data': [
         'data/cookies_policy.xml',
-        'views/portal_template.xml',
+        'views/assets.xml',
+        'views/website_template.xml',
         'views/res_config_settings_views.xml',
-        'views/web_template.xml',
     ],
 }
