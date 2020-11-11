b7072d84d6f6dba2766aa889d9990bb4f7401f6c
Foram <foram.shah@initos.com>
2020-11-11 16:41:35 +0530

    [MIG][website_google_tag_manager]: Migrate module to 14.0

--- website_google_tag_manager/__manifest__.py
+++ website_google_tag_manager/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Google Tag Manager",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "ABF OSIELL, Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
