d7f1d599ae345470d22f40a88dabf47d345a2ab3
Carlos Roca <carlos.roca@tecnativa.com>
2021-02-16 10:04:57 +0100

    [IMP] website_snippet_country_dropdown: black, isort, prettier

--- website_snippet_country_dropdown/__manifest__.py
+++ website_snippet_country_dropdown/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Sergio Teruel <sergio.teruel@tecnativa.com>
 # Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
@@ -9,21 +8,12 @@
     "version": "12.0.1.2.0",
     "category": "Website",
     "website": "http://www.tecnativa.com",
-    "author": "Tecnativa, "
-              "Odoo Community Association (OCA)",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "application": False,
     "installable": True,
-    "depends": [
-        "website",
-    ],
-    'qweb': ['static/src/xml/*.xml'],
-    "data": [
-        "views/assets.xml",
-        "views/snippets.xml",
-    ],
-    "demo": [
-        "demo/pages.xml",
-        "demo/assets.xml",
-    ],
+    "depends": ["website"],
+    "qweb": ["static/src/xml/*.xml"],
+    "data": ["views/assets.xml", "views/snippets.xml"],
+    "demo": ["demo/pages.xml", "demo/assets.xml"],
 }
