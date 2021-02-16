541918e20aee65e241dce72a5e0b77b6c364c090
Carlos Roca <carlos.roca@tecnativa.com>
2021-02-16 10:04:57 +0100

    [IMP] website_snippet_country_dropdown: black, isort, prettier

--- website_snippet_country_dropdown/tests/test_snippet_country_dropdown.py
+++ website_snippet_country_dropdown/tests/test_snippet_country_dropdown.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 import odoo.tests
@@ -8,8 +7,8 @@ class TestUi(odoo.tests.HttpCase):
     def test_01_demo_country_dropdown_tour(self):
         self.phantom_js(
             "/",
-            "odoo.__DEBUG__.services['web_tour.tour']." +
-            "run('website_snippet_country_dropdown_tour_demo_page')",
-            "odoo.__DEBUG__.services['web_tour.tour']" +
-            ".tours.website_snippet_country_dropdown_tour_demo_page.ready"
+            "odoo.__DEBUG__.services['web_tour.tour']."
+            + "run('website_snippet_country_dropdown_tour_demo_page')",
+            "odoo.__DEBUG__.services['web_tour.tour']"
+            + ".tours.website_snippet_country_dropdown_tour_demo_page.ready",
         )
