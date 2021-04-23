562e3692d1c27e560de3f2348ed618f96e96bf3d
lazmas <31168705+lazmas@users.noreply.github.com>
2021-04-24 00:04:56 +0300

    Update res_config_settings.py

--- website_cookiebot/models/res_config_settings.py
+++ website_cookiebot/models/res_config_settings.py
@@ -4,18 +4,14 @@ from odoo import api, fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     cookiebot_id = fields.Char(
-        string='Cookiebot ID',
-        related='website_id.cookiebot_id',
-        readonly=False,
+        string="Cookiebot ID", related="website_id.cookiebot_id", readonly=False,
     )
-    has_cookiebot_id = fields.Boolean(
-        string='Cookiebot',
-    )
+    has_cookiebot_id = fields.Boolean(string="Cookiebot",)
 
-    @api.onchange('has_cookiebot_id')
+    @api.onchange("has_cookiebot_id")
     def onchange_has_cookiebot_id(self):
         if not self.has_cookiebot_id:
             self.cookiebot_id = False
@@ -23,13 +19,11 @@ class ResConfigSettings(models.TransientModel):
     @api.model
     def get_values(self):
         res = super().get_values()
-        get_param = self.env['ir.config_parameter'].sudo().get_param
-        res.update(
-            has_cookiebot_id=get_param('website.has_cookiebot_id'),
-        )
+        get_param = self.env["ir.config_parameter"].sudo().get_param
+        res.update(has_cookiebot_id=get_param("website.has_cookiebot_id"),)
         return res
 
     def set_values(self):
         super().set_values()
-        set_param = self.env['ir.config_parameter'].sudo().set_param
-        set_param('website.has_cookiebot_id', self.has_cookiebot_id)
+        set_param = self.env["ir.config_parameter"].sudo().set_param
+        set_param("website.has_cookiebot_id", self.has_cookiebot_id)
