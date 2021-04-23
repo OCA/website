cb5e14e45682b9b452b49e04a14ea035d6ba3336
lazmas <31168705+lazmas@users.noreply.github.com>
2021-04-23 23:51:14 +0300

    Update __manifest__.py

--- website_cookiebot/__manifest__.py
+++ website_cookiebot/__manifest__.py
@@ -1,18 +1,18 @@
 # Copyright 2020 Trey - Antonio González <antonio@trey.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Website Cookiebot',
-    'summary': 'Cookiebot integration',
-    'category': 'Website',
-    'version': '13.0.1.0.0',
-    'author': 'Trey (www.trey.es), Odoo Community Association (OCA)',
-    'website': 'https://www.trey.es',
-    'license': 'AGPL-3',
-    'depends': ['website', 'portal'],
-    'data': [
-        'data/cookies_policy.xml',
-        'views/assets.xml',
-        'views/website_template.xml',
-        'views/res_config_settings_views.xml',
+    "name": "Website Cookiebot",
+    "summary": "Cookiebot integration",
+    "category": "Website",
+    "version": '13.0.1.0.0',
+    "author": "Trey (www.trey.es), Odoo Community Association (OCA)",
+    "website": "https://www.trey.es",
+    "license": "AGPL-3",
+    "depends": ["website", "portal"],
+    "data": [
+        "data/cookies_policy.xml",
+        "views/assets.xml",
+        "views/website_template.xml",
+        "views/res_config_settings_views.xml",
     ],
 }
