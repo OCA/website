a84beca1a1d60ba3a3f766cc1ca1163f78f191ec
lazmas <31168705+lazmas@users.noreply.github.com>
2021-04-23 22:35:37 +0300

    Create website.py

---
+++ website_cookiebot/models/website.py
@@ -0,0 +1,12 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class Website(models.Model):
+    _inherit = 'website'
+
+    cookiebot_id = fields.Char(
+        string='Cookiebot ID',
+        help='This field holds the ID, needed for Cookiebot functionality.',
+    )
