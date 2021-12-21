3901a64fdee837bca1accf0dd13daf8ef0d542b0
r.duijzers <r.duijzers@onestein.nl>
2021-12-21 11:38:22 +0100

    initial commit

---
+++ website_matomo_tag_manager/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Website Matomo Tag Manager',
+    'category': 'Website',
+    'version': '15.0.1.0.0',
+    'author': 'Onestein',
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'views/website.xml',
+        'views/website_config_settings.xml',
+        'templates/website.xml',
+    ]
+}
