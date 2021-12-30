1cd4d685fe423283c7a4fb2aaa4d586c38c1f898
Alexandre D. Díaz <alexandre.diaz@tecnativa.com>
2021-12-30 22:34:01 +0100

    [MIG] website_snippet_marginless_gallery: black, isort, prettier

--- website_snippet_marginless_gallery/__manifest__.py
+++ website_snippet_marginless_gallery/__manifest__.py
@@ -4,23 +4,16 @@
 {
     "name": "Marginless Gallery Snippet",
     "summary": "Add a snippet to have a marginless image gallery",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Website",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/website",
     "author": "Grupo ESOC Ingeniería de Servicios, "
-              "Tecnativa, "
-              "Odoo Community Association (OCA)",
+    "Tecnativa, "
+    "Odoo Community Association (OCA)",
     "application": False,
     "installable": True,
-    "images": [
-        "images/marginless_gallery.png",
-    ],
-    "depends": [
-        "website",
-    ],
-    "data": [
-        "views/assets.xml",
-        "views/snippets.xml",
-    ],
+    "images": ["images/marginless_gallery.png"],
+    "depends": ["website"],
+    "data": ["views/assets.xml", "views/snippets.xml"],
 }
--- website_snippet_marginless_gallery/static/src/js/website_snippet_marginless_gallery.tour.js
+++ website_snippet_marginless_gallery/static/src/js/website_snippet_marginless_gallery.tour.js
@@ -2,26 +2,33 @@
  * Copyright 2019 Tecnativa - Cristina Martin R.
  * License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html). */
 
-odoo.define('website_snippet_marginless_gallery.tour', function (require) {
-    'use strict';
-    var core = require('web.core');
-    var tour = require('web_tour.tour');
-    var base = require('web_editor.base');
+odoo.define("website_snippet_marginless_gallery.tour", function(require) {
+    "use strict";
+    var core = require("web.core");
+    var tour = require("web_tour.tour");
+    var base = require("web_editor.base");
 
     var _t = core._t;
 
-    tour.register("marginless_gallery", {
-        url: "/",
-        wait_for: base.ready(),
-    }, [{
-        trigger: "a[data-action=edit]",
-        content: _t("<b>Click Edit</b> to start designing your homepage."),
-        extra_trigger: ".homepage",
-        position: "bottom",
-    }, {
-        trigger: "#snippet_feature .oe_snippet:contains('Marginless Gallery')",
-        content: _t("Drag <i>Marginless Gallery</i> and drop it on the page."),
-        position: "bottom",
-        run: "drag_and_drop #wrap",
-    }]);
+    tour.register(
+        "marginless_gallery",
+        {
+            url: "/",
+            wait_for: base.ready(),
+        },
+        [
+            {
+                trigger: "a[data-action=edit]",
+                content: _t("<b>Click Edit</b> to start designing your homepage."),
+                extra_trigger: ".homepage",
+                position: "bottom",
+            },
+            {
+                trigger: "#snippet_feature .oe_snippet:contains('Marginless Gallery')",
+                content: _t("Drag <i>Marginless Gallery</i> and drop it on the page."),
+                position: "bottom",
+                run: "drag_and_drop #wrap",
+            },
+        ]
+    );
 });
