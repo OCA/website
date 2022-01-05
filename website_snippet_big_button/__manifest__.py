94913a8948998d74377015863fc12e4e241b1a83
Guillermo Gallego <guillermo.gallego@tecnativa.com>
2022-01-05 17:14:01 +0100

    [MIG] website_snippet_big_button: Migration to 13.0
    TT27594

--- website_snippet_big_button/__manifest__.py
+++ website_snippet_big_button/__manifest__.py
@@ -1,10 +1,11 @@
 # Copyright 2015-2016 Tecnativa - Jairo Llopis <jairo.llopis@tecnativa.com>
 # Copyright 2019 Tecnativa - Cristina Martin R.
+# Copyright 2022 Tecnativa - Guillermo Gallego <guillermo.gallego@tecnativa.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Big Buttons Snippet",
     "summary": "A snippet that adds two big buttons",
-    "version": "12.0.1.1.0",
+    "version": "13.0.1.0.0",
     "category": "Website",
     "website": "https://github.com/OCA/website",
     "author": "Grupo ESOC Ingeniería de Servicios, S.L.U., "
@@ -13,6 +14,6 @@
     "license": "LGPL-3",
     "application": False,
     "installable": True,
-    "depends": ["website",],
-    "data": ["templates/assets.xml", "templates/snippets.xml",],
+    "depends": ["website"],
+    "data": ["templates/assets.xml", "templates/snippets.xml"],
 }
