0e065557884103ee223782fb786a77036b544222
Raciel Perdomo Gomez <racielperdomo1993@gmail.com>
2022-01-11 17:09:19 -0500

    [IMP] website_field_autocomplete: black, isort, prettier

--- website_field_autocomplete/controllers/main.py
+++ website_field_autocomplete/controllers/main.py
@@ -1,32 +1,32 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import json
+
 from odoo import http
 from odoo.http import request
+
 from odoo.addons.website.controllers.main import Website
 
 
 class Website(Website):
-
     @http.route(
-        '/website/field_autocomplete/<string:model>',
-        type='http',
-        auth='public',
-        methods=['GET'],
+        "/website/field_autocomplete/<string:model>",
+        type="http",
+        auth="public",
+        methods=["GET"],
         website=True,
     )
     def _get_field_autocomplete(self, model, **kwargs):
         """ Return json autocomplete data """
-        domain = json.loads(kwargs.get('domain', "[]"))
-        fields = json.loads(kwargs.get('fields', "[]"))
-        limit = kwargs.get('limit', None)
+        domain = json.loads(kwargs.get("domain", "[]"))
+        fields = json.loads(kwargs.get("fields", "[]"))
+        limit = kwargs.get("limit", None)
         res = self._get_autocomplete_data(model, domain, fields, limit)
         return json.dumps(res.values())
 
     def _get_autocomplete_data(self, model, domain, fields, limit=None):
-        """ Gets and returns raw record data
+        """Gets and returns raw record data
         Params:
             model: Model name to query on
             domain: Search domain
@@ -37,7 +37,5 @@ class Website(Website):
         """
         if limit:
             limit = int(limit)
-        res = request.env[model].search_read(
-            domain, fields, limit=limit
-        )
-        return {r['id']: r for r in res}
+        res = request.env[model].search_read(domain, fields, limit=limit)
+        return {r["id"]: r for r in res}
