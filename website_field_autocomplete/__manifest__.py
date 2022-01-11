d8cdf804460ad3b69d670f1066b51a974c06354f
Raciel Perdomo Gomez <racielperdomo1993@gmail.com>
2022-01-11 17:09:19 -0500

    [IMP] website_field_autocomplete: black, isort, prettier

--- website_field_autocomplete/__manifest__.py
+++ website_field_autocomplete/__manifest__.py
@@ -1,24 +1,23 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Website Field - AutoComplete",
-    "summary": 'Provides an autocomplete field for Website on any model',
+    "summary": "Provides an autocomplete field for Website on any model",
     "version": "10.0.1.0.0",
     "category": "Website",
-    "website": "https://laslabs.com/",
+    "website": "https://github.com/OCA/website",
     "author": "LasLabs, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
-    'installable': True,
+    "installable": True,
     "depends": [
         "website",
     ],
     "data": [
-        'views/assets.xml',
+        "views/assets.xml",
     ],
-    'demo': [
-        'demo/field_autocomplete_demo.xml',
-    ]
+    "demo": [
+        "demo/field_autocomplete_demo.xml",
+    ],
 }
