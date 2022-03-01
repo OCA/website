890a5f7a1a54d5e5fedc4448d1a458c09ef6a236
manu <manuel.regidor@sygel.es>
2022-03-01 16:13:49 +0100

    [ADD]website_membership_gamification: Show user badges in website

---
+++ website_membership_gamification/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2022 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Website Membership Gamification",
+    "summary": "Show badges assigned to users on website",
+    "version": "13.0.1.0.0",
+    "category": "Website",
+    "author": "Sygel, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/website",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "development_status": "Beta",
+    "depends": ["base", "gamification", "contacts", "website_membership"],
+    "data": [
+        "wizard/gamification_badge_user_wizard.xml",
+        "views/badge.xml",
+        "views/res_partner_views.xml",
+        "views/website_partner_template.xml",
+    ],
+}
