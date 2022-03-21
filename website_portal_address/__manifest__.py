00c287c4d6362f505ed0a29b66b5e4bc0131e7d3
Simone Rubino <sir@takobi.online>
2022-03-21 16:15:10 +0100

    [MIG] website_portal_address: Migration to 12.0

--- website_portal_address/__manifest__.py
+++ website_portal_address/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-Today Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 {
     "name": "Contact's Address Manager In Website Portal",
     "summary": "Contact's Address Manager In Website Portal",
-    "version": "9.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Portal",
     "author": """Serpent Consulting Services Pvt. Ltd.,
                 Agile Business Group,
