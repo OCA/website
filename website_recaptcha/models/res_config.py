ceb7fc334ff63cfdf4ea1282d9cf98874620ba63
Victor Champonnois <victor@coopiteasy.be>
2022-04-01 14:29:33 +0200

    [REM] remove has_google_recaptcha field

--- website_recaptcha/models/res_config.py
+++ website_recaptcha/models/res_config.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Simone Orsi - Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class PortalConfigSettings(models.TransientModel):
@@ -9,19 +9,3 @@ class PortalConfigSettings(models.TransientModel):
 
     recaptcha_key_site = fields.Char()
     recaptcha_key_secret = fields.Char()
-
-    has_google_recaptcha = fields.Boolean(
-        "Google reCaptcha",
-        compute="_compute_has_google_recaptcha",
-        inverse="_inverse_has_google_recaptcha",
-        readonly=False,
-    )
-
-    @api.depends("recaptcha_key_site")
-    def _compute_has_google_recaptcha(self):
-        self.has_google_recaptcha = bool(self.recaptcha_key_site)
-
-    def _inverse_has_google_recaptcha(self):
-        if not self.has_google_recaptcha:
-            self.recaptcha_key_site = False
-            self.recaptcha_key_secret = False
