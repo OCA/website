08ff82c89afc42efccff98b5bfb894dd493356ff
Pierre Verkest <pierreverkest84@gmail.com>
2022-05-09 15:22:24 +0200

    [ADD] website_snippet_filter
    This module add Website > Configuration > Dynamic snippet > Snippet filter and
    Website > Configuration Dynamic snippet > Snippet filter template menus
    to let website designer to configure dynamic snippets
    (like the dynamic carousel) in website.

---
+++ website_snippet_filter_views/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Pierre Verkest <pierreverkest84@gmail.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Website Snippet Filter Views",
+    "summary": "Addi website.snippet.filter backend views",
+    "version": "14.0.1.0.0",
+    "category": "Website",
+    "website": "https://github.com/OCA/website",
+    "author": "Pierre Verkest <pierreverkest84@gmail.com>, "
+    "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["website"],
+    "data": ["views/snippet_filter.xml", "views/snippet_filter_template.xml"],
+}
---
+++ website_snippet_filter_views/readme/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+This module add `Website > Configuration > Dynamic snippet > Snippet filter` and
+`Website > Configuration Dynamic snippet > Snippet filter template` menus
+to let website designer to configure dynamic snippets
+(like the dynamic carousel) in website.
