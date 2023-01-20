940601f1a486c00eb15e9addf75a795ccc990d09
Victor Champonnois <victor@coopiteasy.be>
2023-01-20 10:06:48 +0100

    [REF] rename website_analytics_piwik rename website_analytics_matomo

--- website_analytics_piwik/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright 2015 Therp BV <http://therp.nl>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from . import website
-from . import website_config_settings
--- website_analytics_piwik/readme/DESCRIPTION.rst
+++
@@ -1,3 +0,0 @@
-This module allows to simply configure your odoo website for using Piwik_.
-
-.. _Piwik: http://piwik.org
