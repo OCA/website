d91ec14d599cbe5b73ce0659d595071e0f288a43
Carlos Roca <carlos.roca@tecnativa.com>
2023-02-23 13:01:27 +0100

    [IMP] website_legal_page: Remove redirection
    Remove redirection of /terms to /legal but maintain module to
    complement account terms or to set legal terms to pages that
    has not account installed.

--- website_legal_page/__init__.py
+++ website_legal_page/__init__.py
@@ -1,4 +1,1 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import controllers
-from .hooks import post_init_hook, _merge_views
--- website_legal_page/__manifest__.py
+++ website_legal_page/__manifest__.py
@@ -8,19 +8,12 @@
     "version": "15.0.1.0.1",
     "depends": ["website"],
     "data": [
-        "views/reusable_templates.xml",
         "views/website_legal_main_page.xml",
     ],
-    "assets": {
-        "web.assets_frontend": [
-            "/website_legal_page/static/src/css/website_legal_page.scss"
-        ]
-    },
     "author": "Tecnativa, "
     "LasLabs, "
     "Nicolas JEUDY, "
     "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/website",
     "license": "AGPL-3",
-    "post_init_hook": "post_init_hook",
 }
