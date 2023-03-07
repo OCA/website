26015f7042737bb4d3f77609d65edbec88581f96
miguels73 <55379877+miguels73@users.noreply.github.com>
2023-03-07 12:25:09 +0100

    [14.0][ADD] website_google_analytics_4: integration to 4 generation GA

---
+++ website_google_analytics_4/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Studio73 - Miguel Gandia <miguel@studio73.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Google Analytics 4",
+    "summary": "Google Analytics 4 integration",
+    "category": "Website",
+    "version": "14.0.1.0.0",
+    "author": "Studio73, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/website",
+    "license": "AGPL-3",
+    "depends": ["website_sale"],
+    "data": ["views/assets.xml", "views/website_templates.xml"],
+    "installable": True,
+}
