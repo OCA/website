b9bcfea2e84fc5e841f71283dd31e2784f710d9f
Carlos Roca <carlos.roca@tecnativa.com>
2023-03-09 16:22:06 +0100

    [ADD] website_form_require_legal: New module to add terms acceptance at website form level

---
+++ website_form_require_legal/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2023 Tecnativa - Carlos Roca
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "Website Form Require Legal",
+    "summary": "Add possibility to require confirm legal terms.",
+    "version": "15.0.1.0.0",
+    "category": "Website",
+    "license": "LGPL-3",
+    "website": "https://github.com/OCA/website",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "application": False,
+    "installable": True,
+    "depends": ["website"],
+    "data": ["views/snippets.xml"],
+    "assets": {
+        "website.assets_wysiwyg": [
+            "website_form_require_legal/static/src/js/options.js"
+        ],
+        "web.assets_frontend": [
+            "website_form_require_legal/static/src/scss/website_form_legal.scss"
+        ],
+    },
+}
---
+++ website_form_require_legal/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module adds the possibility to require confirm legal terms at frontend forms.
