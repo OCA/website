88eacc7f8f2b938b237563e52861346ea667d044
JoanSoleForgeflow <joan.sole@forgeflow.com>
2023-06-06 13:29:08 +0200

    [ADD] website_active: enable to archive websites

---
+++ website_active/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Website Active",
+    "version": "14.0.1.0.0",
+    "author": "Forgeflow, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Website",
+    "summary": "Enable to archive websites",
+    "website": "https://github.com/OCA/website",
+    "depends": [
+        "website",
+    ],
+    "installable": True,
+}
