a58b3346340544082eb4d742faee36bff882314e
Andrea Stirpe <a.stirpe@onestein.nl>
2023-07-03 09:42:53 +0200

    [16.0][MIG] website_analytics_matomo

--- website_analytics_matomo/__manifest__.py
+++ website_analytics_matomo/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Matomo analytics",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Onestein,Therp BV,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Website",
