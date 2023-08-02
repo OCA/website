571c8e8a634b75a49c52c7c22732876f4cec12f6
Jordi Ballester Alomar <jordi.ballester@forgeflow.com>
2023-08-02 10:22:34 +0200

    [ADD] website_search_header: allow to search from within the search header

---
+++ website_search_header/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2023 ForgeFlow, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
+
+{
+    "name": "Website Search in Header",
+    "category": "Website",
+    "version": "16.0.1.0.0",
+    "author": "ForgeFlow, Ooops, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/website",
+    "license": "LGPL-3",
+    "depends": [
+        "website",
+        "web",
+    ],
+    "data": [
+        "views/templates.xml",
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "website_search_header/static/src/scss/website_search_header.scss",
+        ],
+    },
+    "installable": True,
+}
