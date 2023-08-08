559d2ac262fea8644e024b4f49b5182adee01b4b
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2023-08-08 17:40:47 +0200

    [MIG] website_form_require_legal: Migration to version 16.0
    TT44376
    
    [UPD] Update website_form_require_legal.pot
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: website-16.0/website-16.0-website_form_require_legal
    Translate-URL: https://translation.odoo-community.org/projects/website-16-0/website-16-0-website_form_require_legal/

--- website_form_require_legal/__manifest__.py
+++ website_form_require_legal/__manifest__.py
@@ -3,18 +3,19 @@
 {
     "name": "Website Form Require Legal",
     "summary": "Add possibility to require confirm legal terms.",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Website",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/website",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "application": False,
     "installable": True,
-    "depends": ["website"],
+    "depends": ["web_editor", "website"],
     "data": ["views/snippets.xml"],
     "assets": {
         "website.assets_wysiwyg": [
-            "website_form_require_legal/static/src/js/options.js"
+            "website_form_require_legal/static/src/js/options.js",
+            "website_form_require_legal/static/src/xml/website_form_editor.xml",
         ],
         "web.assets_frontend": [
             "website_form_require_legal/static/src/scss/website_form_legal.scss"
