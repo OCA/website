2e8e371d12c042a8e89e02b47cf55dd21f8aefec
Elmer Garcia Yavi <elmer.garcia@forgeflow.com>
2023-10-16 09:46:12 +0200

    [ADD] website_default_code_product: display the default code attribute of the product in both the GRID/LIST views and the product details

---
+++ website_default_code_product/README.rst
@@ -0,0 +1,4 @@
+============================
+Website Default Code Product
+============================
+This module displays the default code attribute of the product in both the GRID/LIST views and the product details.
---
+++ website_default_code_product/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Website Default Code Product",
+    "category": "Website",
+    "version": "16.0.1.0.0",
+    'author': "ForgeFlow",
+    "license": "LGPL-3",
+    "depends": [
+        "website_sale"
+    ],
+    "data": [
+        "views/website_default_code_product_views.xml",
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "website_default_code_product/static/src/scss/website_default_code_product.scss",
+        ],
+    },
+    "installable": True,
+}
