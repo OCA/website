170c53a9d914b557212273021c39b1a4919cce52
Elmer Garcia Yavi <elmer.garcia@forgeflow.com>
2023-10-23 17:10:24 +0200

    [UPDATE] website_default_code: add label SKU and applies styling

--- website_default_code_product/__manifest__.py
+++ website_default_code_product/__manifest__.py
@@ -2,11 +2,10 @@
     "name": "Website Default Code Product",
     "category": "Website",
     "version": "16.0.1.0.0",
-    'author': "ForgeFlow",
+    "website": "https://github.com/OCA/website",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
     "license": "LGPL-3",
-    "depends": [
-        "website_sale"
-    ],
+    "depends": ["website_sale"],
     "data": [
         "views/website_default_code_product_views.xml",
     ],
