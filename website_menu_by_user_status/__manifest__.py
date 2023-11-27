674ca1573c67b551774dda81a20feb6023ecee01
Nils Coenen <74965194+NICO-SOLUTIONS@users.noreply.github.com>
2023-11-27 14:01:08 +0100

    [MIG] website_menu_by_user_status: Migration to 17.0

--- website_menu_by_user_status/__manifest__.py
+++ website_menu_by_user_status/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Website Menu By User Display",
-    "version": "14.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Savoir-faire Linux,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/website",
     "license": "AGPL-3",
--- website_menu_by_user_status/tests/test_website_menu.py
+++ website_menu_by_user_status/tests/test_website_menu.py
@@ -1,10 +1,10 @@
 # Copyright 2018 David Dufresne
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests import SavepointCase
+from odoo.tests import TransactionCase
 
 
-class TestWebsiteMenu(SavepointCase):
+class TestWebsiteMenu(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
