1eacd9fa4a3b01ae0b3f7a48637493c83d5069c4
SodexisTeam <dev@sodexis.com>
2024-01-09 13:19:47 +0530

    [MIG] website_google_tag_manager: Migration to 17.0

--- website_google_tag_manager/__manifest__.py
+++ website_google_tag_manager/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Google Tag Manager",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "ABF OSIELL, Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
