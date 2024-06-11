4380b5d56337f4c224f921507688d39b23c2200f
Simone Rubino <simone.rubino@aion-tech.it>
2024-06-11 14:47:22 +0200

    [REF] website_require_login: Extract path matching Also create only one instance of Path(path)

--- website_require_login/README.rst
+++ website_require_login/README.rst
@@ -65,6 +65,9 @@ Contributors
 ~~~~~~~~~~~~
 
 * Ooops404 <https://ooops404.com>
+* `Aion Tech <https://aiontech.company/>`_:
+
+  * Simone Rubino <simone.rubino@aion-tech.it>
 
 Maintainers
 ~~~~~~~~~~~
--- website_require_login/models/ir_http.py
+++ website_require_login/models/ir_http.py
@@ -1,4 +1,5 @@
 # Copyright 2020 Advitus MB
+# Copyright 2024 Simone Rubino - Aion Tech
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
 from pathlib import Path
 
@@ -24,6 +25,18 @@ class IrHttp(models.AbstractModel):
         return super()._serve_fallback()
 
     @classmethod
+    def _require_login_get_matching_path(cls, path, search_paths):
+        """Return which one of `search_paths` is a parent of `path`."""
+        path_inst = Path(path)
+        for search_path in search_paths:
+            if search_path == path or Path(search_path) in path_inst.parents:
+                matching_path = search_path
+                break
+        else:
+            matching_path = None
+        return matching_path
+
+    @classmethod
     def _check_require_auth(cls):
         # if not website request - skip
         website = request.env["website"].sudo().get_current_website()
@@ -42,7 +55,7 @@ class IrHttp(models.AbstractModel):
             .mapped("path")
         )
         path = request.httprequest.path
-        for auth_path in auth_paths:
-            if auth_path == path or Path(auth_path) in Path(path).parents:
-                redirect_path = "/web/login?redirect=%s" % path
-                return request.redirect(redirect_path, code=302)
+        auth_path = cls._require_login_get_matching_path(path, auth_paths)
+        if auth_path:
+            redirect_path = "/web/login?redirect=%s" % path
+            return request.redirect(redirect_path, code=302)
