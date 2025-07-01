f56363eb003f05e4c51692c3f5cc604176502cd2
Víctor Martínez <victor.martinez@tecnativa.com>
2025-07-01 08:54:45 +0200

    [MIG] website_legal_page: Migration to 18.0
    TT54870

--- website_legal_page/__manifest__.py
+++ website_legal_page/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright 2015 Antonio Espinosa <antonioea@antiun.com>
-# Copyright 2017 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2017 Tecnativa - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Website Legal Page",
     "category": "Website",
-    "version": "16.0.1.1.0",
+    "version": "18.0.1.0.0",
     "depends": ["website"],
     "data": [
         "views/website_legal_main_page.xml",
