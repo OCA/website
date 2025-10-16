b369bbc177f1ba080f38f77774d12846598b98b1
eduardo <eduardo.ezerouali@tecnativa.com>
2025-10-16 15:09:16 +0000

    [MIG] website_menu_by_user_status: Migration to 18.0

--- website_menu_by_user_status/tests/test_website_menu.py
+++ website_menu_by_user_status/tests/test_website_menu.py
@@ -1,10 +1,10 @@
 # Copyright 2018 David Dufresne
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests import TransactionCase
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class TestWebsiteMenu(TransactionCase):
+class TestWebsiteMenu(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
