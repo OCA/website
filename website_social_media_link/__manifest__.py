e3981075bd2a32cc3e5254da27fd509d4577ba9e
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-03-08 20:37:22 +0100

    [ADD] website_social_media_link. This module adds Social Medial links on websites back-end form view. This module is a bugfix. In odoo, when creating a new website, the URLs of the social media are copied from 'res.company' model to 'website' model. Once done, it is not possible to user to change data on 'website' model, if an URL change, or a new social media is used.

---
+++ website_social_media_link/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2026 - Today: GRAP https://www.grap.coop
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "Website - Company Mastodon Link",
+    "summary": "Display Social Medial Links on Website" " back-end.",
+    "version": "18.0.1.0.0",
+    "author": "GRAP, Odoo Community Association (OCA)",
+    "maintainers": ["legalsylvain"],
+    "website": "https://github.com/OCA/website",
+    "license": "AGPL-3",
+    "category": "Website",
+    "depends": ["website"],
+    "data": ["views/view_website.xml"],
+}
