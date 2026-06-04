9339bd155a2dcfc7d33f669f8d93f094cfbd0045
Juan Arcos <j.arcos@madetosoft.com>
2026-06-04 11:24:37 +0200

    [ADD] website_sale_vat_label: Introduction of country-specific VAT labels
    - Create the new module to dynamically update the VAT field label in the frontend address form.
    - Override `_prepare_address_form_values` in `WebsiteSale` controller to fetch `country_sudo.vat_label`.
    - Add module documentation (description, context, usage, and history) following OCA guidelines.

---
+++ website_sale_country_vat_label/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 MTS - Juan Arcos
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Website Sale Country VAT Label",
+    "summary": "Use country specific VAT label in website address form",
+    "version": "18.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Website",
+    "website": "https://github.com/OCA/website",
+    "author": "MTS, Odoo Community Association (OCA)",
+    "maintainers": ["jarcosmts"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "preloadable": False,
+    "depends": ["website_sale"],
+}
---
+++ website_sale_country_vat_label/controllers/main.py
@@ -0,0 +1,21 @@
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+
+class WebsiteSaleVatLabel(WebsiteSale):
+
+    def _prepare_address_form_values(self, *args, **kwargs):
+        # 1. Call the original function to load all the native values
+        res = super()._prepare_address_form_values(*args, **kwargs)
+
+        # 2. We obtain the current country from the values ​​already prepared by Odoo
+        country_sudo = res.get("country")
+
+        # 3. Modify the 'res' dictionary with the country specific VAT label, if the country exists
+        if country_sudo:
+            res["vat_label"] = country_sudo.vat_label or request.env._("VAT")
+
+        return res
