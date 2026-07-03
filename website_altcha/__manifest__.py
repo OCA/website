4c582b7298d61c9da20ade77ad92576d0a02a8e7
Holger Brunn <mail@hunki-enterprises.com>
2026-07-03 12:24:57 +0200

    [IMP] website_altcha: add hbrunn as maintainer

--- website_altcha/__manifest__.py
+++ website_altcha/__manifest__.py
@@ -8,6 +8,7 @@
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Dixmit,Odoo Community Association (OCA)",
+    "maintainers": ["hbrunn"],
     "website": "https://github.com/OCA/website",
     "depends": [
         "website",
